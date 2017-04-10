a2fb07a7b19bf5c75fd603aa35ffa536f36b0ca1
Roman Kh <rhudor@gmail.com>
2017-04-10 15:35:36 +0300

    Added nogil

--- dataset/decorators.py
+++ dataset/decorators.py
@@ -10,8 +10,8 @@ def action(method):
 
 def within_parallel(init, post=None, target='threads'):
     """ Make within-batch parallel decorator """
-    if target not in ['threads', 'mpc', 'async', 'dd']:
-        raise ValueError("target should one of 'threads', 'mpc', 'async', 'dd'")
+    if target not in ['nogil', 'threads', 'mpc', 'async', 'dd']:
+        raise ValueError("target should one of 'nogil', threads', 'mpc', 'async', 'dd'")
 
     def within_parellel_decorator(method):
         """ Return a decorator which run a method in parallel """
@@ -32,7 +32,18 @@ def within_parallel(init, post=None, target='threads'):
                 post_fn = None
             return init_fn, post_fn
 
-        def wrap_with_threads(self, *args, **kwargs):
+        def make_args(arg):
+            if isinstance(arg, tuple) and len(arg) == 2:
+                margs, mkwargs = arg
+            elif isinstance(arg, dict):
+                margs = []
+                mkwargs = arg
+            else:
+                mkwargs = dict()
+                margs = arg
+            return margs, mkwargs
+
+        def wrap_with_threads(self, args, kwargs, nogil=False):
             """ Run a method in parallel """
             init_fn, post_fn = check_functions(self)
 
@@ -40,15 +51,13 @@ def within_parallel(init, post=None, target='threads'):
             with cf.ThreadPoolExecutor(max_workers=n_workers) as executor:
                 futures = []
                 for arg in init_fn(self, *args, **kwargs):
-                    if isinstance(arg, tuple) and len(args) == 2:
-                        margs, mkwargs = arg
-                    elif isinstance(arg, dict):
-                        margs = []
-                        mkwargs = arg
+                    margs, mkwargs = make_args(arg)
+                    if nogil:
+                        nogil_fn = method(self)
+                        one_ft = executor.submit(nogil_fn, *margs, **mkwargs)                        
                     else:
-                        mkwargs = dict()
-                        margs = arg
-                    futures.append(executor.submit(method, self, *margs, **mkwargs))
+                        one_ft = executor.submit(method, self, *margs, **mkwargs)                        
+                    futures.append(one_ft)
                 timeout = kwargs.get('timeout', 1000)
                 done, not_done = cf.wait(futures, timeout=timeout, return_when=cf.ALL_COMPLETED)
 
@@ -58,10 +67,14 @@ def within_parallel(init, post=None, target='threads'):
                 done_results = [done_f.result() for done_f in done]
                 return post_fn(done_results, not_done)
 
+
         def wrapped_method(self, *args, **kwargs):
             """ Wrap a method in a required parallel engine """
             if target == 'threads':
-                return wrap_with_threads(self, *args, **kwargs)
+                return wrap_with_threads(self, args, kwargs)
+            elif target == 'nogil':
+                return wrap_with_threads(self, args, kwargs, nogil=True)
+
             raise ValueError('Wrong parallelization target:', target)
         return wrapped_method
     return within_parellel_decorator
