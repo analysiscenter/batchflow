66d3a7d86bb3f329e9c54b9606c5e8d433e7d78b
Roman Kh <rhudor@gmail.com>
2017-06-09 01:27:18 +0300

    Added base openset class

---
+++ dataset/opensets/base.py
@@ -0,0 +1,44 @@
+""" Contains MNIST dataset """
+
+import os
+import tempfile
+import urllib
+import gzip
+import numpy as np
+
+from .. import Dataset, DatasetIndex, ImagesBatch
+
+
+class Openset:
+    """ The base class for open datasets """
+    def __init__(self, batch_class):
+        self.batch_class = batch_class
+        self.train = None
+        self.test = None
+        self._data = self.download()
+        self.create_datasets(preloaded=self._data is not None)
+
+    def download(self):
+    	""" Download a dataset from the source web-site """
+    	return None
+
+    def create_datasets(self):
+        raise NotImplementedError()
+
+
+class ImagesOpenset(Openset):
+    """ The base class for open datasets with images """
+    def __init__(self, batch_class=ImagesBatch):
+        super().__init__(batch_class)
+
+    def create_datasets(self, preloaded=False):
+        """ Create train and test datasets """
+        if isinstance(self._data, tuple) and len(self._data) == 2:
+            train_data, test_data = self._data
+            train_index = DatasetIndex(np.arange(len(train_data[0])))
+            train_preloaded = train_data if preloaded else None
+            self.train = Dataset(train_index, self.batch_class, preloaded=train_preloaded)
+
+            test_index = DatasetIndex(np.arange(len(test_data[0])))
+            test_preloaded = test_data if preloaded else None
+            self.test = Dataset(test_index, self.batch_class, preloaded=test_preloaded)
--- dataset/opensets/cifar.py
+++ dataset/opensets/cifar.py
@@ -1,4 +1,4 @@
-""" Contains MNIST dataset """
+""" Contains CIFAR datasets """
 
 import os
 import tempfile
@@ -7,31 +7,17 @@ import pickle
 import tarfile
 import numpy as np
 
-from .. import Dataset, DatasetIndex, ImagesBatch
+from . import ImagesOpenset
 
 
-class BaseCIFAR:
-    """ CIFAR dataset """
+class BaseCIFAR(ImagesOpenset):
+    """ The base class for the CIFAR dataset """
     SOURCE_URL = None
     LABELS_KEY = None
     TRAIN_NAME_ID = None
     TEST_NAME_ID = None
 
-    def __init__(self, batch_class=None):
-        train, test = self.download(self.SOURCE_URL)
-        self._train_images, self._train_labels = train
-        self._test_images, self._test_labels = test
-
-        batch_class = batch_class if batch_class is not None else ImagesBatch
-
-        train_index = DatasetIndex(np.arange(len(self._train_images)))
-        self.train = Dataset(train_index, batch_class, preloaded=(self._train_images, self._train_labels))
-
-        test_index = DatasetIndex(np.arange(len(self._test_images)))
-        self.test = Dataset(test_index, batch_class, preloaded=(self._test_images, self._test_labels))
-
-
-    def download(self, url):
+    def download(self):
         """ Load data from a web site and extract into numpy arrays """
 
         def _extract(archive_file, member):
@@ -43,7 +29,7 @@ class BaseCIFAR:
             return images, labels
 
         tmpdir = tempfile.gettempdir()
-        filename = os.path.basename(url)
+        filename = os.path.basename(self.SOURCE_URL)
         localname = os.path.join(tmpdir, filename)
         if not os.path.isfile(localname):
             print("Downloading", filename, "...")
@@ -56,14 +42,13 @@ class BaseCIFAR:
 
             data_files = [one_file for one_file in files_in_archive if self.TRAIN_NAME_ID in one_file.name]
             all_res = [_extract(archive_file, one_file) for one_file in data_files]
-            train = _gather_extracted(all_res)
+            train_data = _gather_extracted(all_res)
 
             test_files = [one_file for one_file in files_in_archive if self.TEST_NAME_ID in one_file.name]
             all_res = [_extract(archive_file, one_file) for one_file in test_files]
-            test = _gather_extracted(all_res)
+            test_data = _gather_extracted(all_res)
         print("Extracted")
-
-        return train, test
+        return train_data, test_data
 
 
 class CIFAR10(BaseCIFAR):
