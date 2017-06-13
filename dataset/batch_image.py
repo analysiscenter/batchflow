64c74e5a779eeaaa27ec808166f61d80b3d0d5b6
Roman Kh <rhudor@gmail.com>
2017-06-13 20:24:05 +0300

    Defined new_attr

--- dataset/batch_image.py
+++ dataset/batch_image.py
@@ -26,6 +26,10 @@ from .decorators import action, inbatch_parallel, any_action_failed
 
 class ImagesBatch(Batch):
     """ Batch class for 2D images """
+    def __init__(self, index, preloaded=None):
+        super().__init__(index, preloaded)
+        self._new_attr = None
+
     @property
     def data(self):
         data = super().data
@@ -68,23 +72,24 @@ class ImagesBatch(Batch):
         self._data = data
 
     def assemble(self, all_res, *args, **kwargs):
+        """ Assemble the batch after a parallel action """
         _ = args, kwargs
         if any_action_failed(all_res):
             raise ValueError("Could not assemble the batch", self.get_errors(all_res))
 
-        dst = kwargs.get('dst', 'images')
+        attr = kwargs.get('attr', 'images')
         if isinstance(all_res[0], PIL.Image.Image):
-            setattr(self, dst, all_res)
+            setattr(self, attr, all_res)
         else:
-            setattr(self, dst, np.transpose(np.dstack(all_res), (2, 0, 1)))
+            setattr(self, attr, np.transpose(np.dstack(all_res), (2, 0, 1)))
         return self
 
     @action
-    def convert_to_PIL(self, dst='images'):
+    def convert_to_PIL(self, attr='images'):   # pylint: disable=invalid-name
         """ Convert batch data to PIL.Image format """
         self._new_attr = list(None for _ in self.indices)
-        self.apply_transform(dst, '_new_attr', PIL.Image.fromarray)
-        setattr(self, dst, self._new_attr)
+        self.apply_transform(attr, '_new_attr', PIL.Image.fromarray)
+        setattr(self, attr, self._new_attr)
         return self
 
     @action
