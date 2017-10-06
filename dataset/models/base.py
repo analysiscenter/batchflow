4c336ef61351e92e21dadad48deb5af78687d73b
Roman Kh <rhudor@gmail.com>
2017-10-06 13:31:52 +0300

    Masked model methods args, kwargs

--- dataset/models/base.py
+++ dataset/models/base.py
@@ -8,20 +8,20 @@ class BaseModel:
 
     def build(self, *args, **kwargs):
         """ Define the model """
-        return None
+        _ = self, args, **kwargs
 
     def load(self, *args, **kwargs):
         """ Load the model """
-        return None
+        _ = self, args, **kwargs
 
     def save(self, *args, **kwargs):
         """ Save the model """
-        return None
+        _ = self, args, **kwargs
 
     def train(self, *args, **kwargs):
         """ Train the model """
-        return None
+        _ = self, args, **kwargs
 
     def predict(self, *args, **kwargs):
         """ Make a prediction using the model  """
-        return None
+        _ = self, args, **kwargs
