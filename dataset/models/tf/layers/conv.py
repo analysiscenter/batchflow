87b8b46a69004d008c8e4a19b6965e6b4cbe3d91
a.kozhevin <a.kozhevin@analysiscenter.ru>
2017-10-20 17:58:44 +0300

    add conv1d_transpose

--- dataset/models/tf/layers/conv.py
+++ dataset/models/tf/layers/conv.py
@@ -1,10 +1,30 @@
 """ Contains convolution layers """
 import tensorflow as tf
 
+def conv1d_transpose(tensor, filters, kernel_size, strides, padding, use_bias):
+    """ Transposed 1D convolution layer
+
+    Parameters
+    ----------
+    input_tensor : tf.Tensor
+    filters : int - number of filters in the ouput tensor
+    kernel_size : int - kernel size
+    strides : int
+    padding : str - padding mode, can be 'same' or 'valid'
+    use_bias : bool
+
+    """
+
+    up_tensor = tf.expand_dims(tensor, 1)
+    conv_output = tf.layers.conv2d_transpose(up_tensor, filters=filters, kernel_size=(1, kernel_size), 
+                                      strides=(1, strides), padding=padding, use_bias=use_bias)
+    output = tf.squeeze(conv_output, [1])
+    return output
+
 ND_LAYERS = {
     'conv': [tf.layers.conv1d, tf.layers.conv2d, tf.layers.conv3d],
     'batch-norm': tf.layers.batch_normalization,
-    'transposed-conv': [None, tf.layers.conv2d_transpose, tf.layers.conv3d_transpose],
+    'transposed-conv': [conv1d_transpose, tf.layers.conv2d_transpose, tf.layers.conv3d_transpose],
     'max-pooling': [tf.layers.max_pooling1d, tf.layers.max_pooling2d, tf.layers.max_pooling3d],
     'dropout': tf.layers.dropout
 }
