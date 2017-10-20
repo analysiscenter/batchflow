3bb3f4fcf643da2f143b7fb47cf1ecfad70842fe
Roman Kh <rhudor@gmail.com>
2017-10-20 10:22:31 +0300

    Added general conv block

--- dataset/models/tf/layers/conv.py
+++ dataset/models/tf/layers/conv.py
@@ -1,4 +1,4 @@
-""" Contains 2d conv layers """
+""" Contains convolution layers """
 import tensorflow as tf
 
 
@@ -9,22 +9,22 @@ def conv1d_block(input_tensor, filters, kernel_size, layout='cnap', name=None,
 
     Parameters
     ----------
-    input_tensor: tf.Tensor
-    filters: int - number of filters in the ouput tensor
-    kernel_size: int - kernel size
-    layout: str - a sequence of layers
+    input_tensor : tf.Tensor
+    filters : int - number of filters in the ouput tensor
+    kernel_size  int - kernel size
+    layout : str - a sequence of layers
         c - convolution
         n - batch normalization
         a - activation
         p - max pooling
-    name: str -  name of the layer that will be used as a scope
-    strides: int
-    padding: str - padding mode, can be 'same' or 'valid';
-    use_bias: bool
-    activation: callable
-    pool_size: int
-    pool_strides: int
-    is_training: bool or tf.Tensor
+    name : str -  name of the layer that will be used as a scope
+    strides : int
+    padding : str - padding mode, can be 'same' or 'valid';
+    use_bias : bool
+    activation : callable
+    pool_size : int
+    pool_strides : int
+    is_training : bool or tf.Tensor
 
     Returns
     -------
@@ -60,22 +60,23 @@ def conv2d_block(input_tensor, filters, kernel_size, layout='cnap', name=None,
 
     Parameters
     ----------
-    input_tensor: tf.Tensor
-    filters: int - number of filters in the ouput tensor
-    kernel_size: int or tuple(int, int) - kernel size
-    layout: str - a sequence of layers
+    input_tensor : tf.Tensor
+    filter s: int - number of filters in the ouput tensor
+    kernel_size:  int or tuple(int, int) - kernel size
+    layout : str - a sequence of layers
         c - convolution
+        t - transposed convolution
         n - batch normalization
         a - activation
         p - max pooling
-    name: str -  name of the layer that will be used as a scope
-    strides: tuple(int, int)
-    padding: str - padding mode, can be 'same' or 'valid';
-    use_bias: bool
-    activation: callable
-    pool_size: tuple(int, int)
-    pool_strides: tuple(int, int)
-    is_training: bool or tf.Tensor
+    name : str -  name of the layer that will be used as a scope
+    strides : tuple(int, int)
+    padding : str - padding mode, can be 'same' or 'valid';
+    use_bias : bool
+    activation : callable
+    pool_size : tuple(int, int)
+    pool_strides : tuple(int, int)
+    is_training : bool or tf.Tensor
 
     Returns
     -------
@@ -113,22 +114,23 @@ def conv3d_block(input_tensor, filters, kernel_size, layout='cnap', name=None,
 
     Parameters
     ----------
-    input_tensor: tf.Tensor
-    filters: int - number of filters in the ouput tensor
-    kernel_size: int or tuple(int, int, int) - kernel size
-    layout: str - a sequence of layers
+    input_tensor : tf.Tensor
+    filters : int - number of filters in the ouput tensor
+    kernel_size : int or tuple(int, int, int) - kernel size
+    layout : str - a sequence of layers
         c - convolution
+        t - transposed convolution
         n - batch normalization
         a - activation
         p - max pooling
-    name: str -  name of the layer that will be used as a scope
-    strides: tuple(int, int, int)
-    padding: str - padding mode, can be 'same' or 'valid';
-    use_bias: bool
-    activation: callable
-    pool_size: tuple(int, int, int)
-    pool_strides: tuple(int, int, int)
-    is_training: bool or tf.Tensor
+    name : str -  name of the layer that will be used as a scope
+    strides : tuple(int, int, int)
+    padding : str - padding mode, can be 'same' or 'valid';
+    use_bias : bool
+    activation : callable
+    pool_size : tuple(int, int, int)
+    pool_strides : tuple(int, int, int)
+    is_training : bool or tf.Tensor
 
     Returns
     -------
@@ -158,3 +160,50 @@ def conv3d_block(input_tensor, filters, kernel_size, layout='cnap', name=None,
         context.__exit__(None, None, None)
 
     return tensor
+
+def conv_block(dim, input_tensor, filters, kernel_size, layout='cnap', name=None,
+                 strides=(1, 1), padding='same', use_bias=True, activation=tf.nn.relu,
+                 pool_size=(2, 2), pool_strides=(2, 2), is_training=True):
+    """ Complex convolution with batch normalization, activation and pooling layers
+
+    Parameters
+    ----------
+    d : int {1, 2, 3} - number of dimensions
+    input_tensor : tf.Tensor
+    filters : int - number of filters in the ouput tensor
+    kernel_size : int or tuple of ints - kernel size
+    layout : str - a sequence of layers
+        c - convolution
+        t - transposed convolution
+        n - batch normalization
+        a - activation
+        p - max pooling
+    name : str -  name of the layer that will be used as a scope
+    strides : int or tuple of ints
+    padding : str - padding mode, can be 'same' or 'valid';
+    use_bias : bool
+    activation : callable
+    pool_size : int or tuple of ints
+    pool_strides : int or tuple of ints
+    is_training : bool or tf.Tensor
+
+    Returns
+    -------
+    output tensor: tf.Tensor
+    """
+    if dim == 1:
+        output = conv1d_block(input_tensor, filters, kernel_size, layout, name,
+                              strides, padding, use_bias, activation,
+                              pool_size, pool_strides, is_training)
+    elif dim == 2:
+        output = conv2d_block(input_tensor, filters, kernel_size, layout, name,
+                              strides, padding, use_bias, activation,
+                              pool_size, pool_strides, is_training)
+    elif dim == 2:
+        output = conv3d_block(input_tensor, filters, kernel_size, layout, name,
+                              strides, padding, use_bias, activation,
+                              pool_size, pool_strides, is_training)
+    else:
+        raise ValueError("Number of dimensions could be 1, 2 or 3, but given %d" % dim)
+
+    return output
