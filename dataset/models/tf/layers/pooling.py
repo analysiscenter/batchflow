065d50dd2750b9ccfdefd09a509cf0f81ab8f22a
anton-br <ziliboba753@gmail.com>
2017-10-27 18:06:13 +0300

    Add max_pooling and average_pooling in pooling.py

---
+++ dataset/models/tf/layers/pooling.py
@@ -0,0 +1,57 @@
+""" Contains pooling layers """
+import tensorflow as tf
+
+def max_pooling(dim, inputs, pool_size, strides, padding='valid', data_format='channels_last', name=None):
+    """ Multi-dimensional max-pooling layer.
+    See all doc here: https://www.tensorflow.org/api_docs/python/tf/layers/max_pooling1d
+
+    Parameters
+    ----------
+    dim: int {1, 2, 3}
+    inputs: tf.Tensor
+    pool_size: int
+    strides: int
+    padding: str {'same', 'valid'}
+    data_format: str {'channels_last', 'channels_first'}
+    name: str
+
+    Returns
+    -------
+    tf.Tensor
+    """
+    if dim == 1:
+        return tf.layers.max_pooling1d(inputs, pool_size, strides, padding, data_format, name)
+    elif dim == 2:
+        return tf.layers.max_pooling2d(inputs, pool_size, strides, padding, data_format, name)
+    elif dim == 3:
+        return tf.layers.max_pooling3d(inputs, pool_size, strides, padding, data_format, name)
+    else:
+        raise ValueError("Number of dimensions could be 1, 2 or 3, but given %d" % dim)
+
+def average_pooling(dim, inputs, pool_size, strides, padding='valid', data_format='channels_last', name=None):
+    """ Multi-dimensional average-pooling layer.
+    See all doc here: https://www.tensorflow.org/api_docs/python/tf/layers/average_pooling1d
+
+    Parameters
+    ----------
+    dim: int {1, 2, 3}
+    inputs: tf.Tensor
+    pool_size: int
+    strides: int
+    padding: str {'same', 'valid'}
+    data_format: str {'channels_last', 'channels_first'}
+    name: str
+
+    Returns
+    -------
+    tf.Tensor
+    """
+    if dim == 1:
+        return tf.layers.average_pooling1d(inputs, pool_size, strides, padding, data_format, name)
+    elif dim == 2:
+        return tf.layers.average_pooling2d(inputs, pool_size, strides, padding, data_format, name)
+    elif dim == 3:
+        return tf.layers.average_pooling3d(inputs, pool_size, strides, padding, data_format, name)
+    else:
+        raise ValueError("Number of dimensions could be 1, 2 or 3, but given %d" % dim)
+
