c786d1c50fcc3416e2d684897ddd9501248fc4e1
Roman Kh <rhudor@gmail.com>
2017-11-05 00:46:32 +0300

    Add separable conv 1d and 3d

--- dataset/models/tf/layers/conv.py
+++ dataset/models/tf/layers/conv.py
@@ -1,246 +1,196 @@
-""" Contains convolution layers """
+""" Contains convolutional layers """
 import tensorflow as tf
 
-from .core import mip
-from .conv1d_tr import conv1d_transpose
-from .pooling import max_pooling, average_pooling, global_max_pooling, global_average_pooling
 
-
-ND_LAYERS = {
-    'activation': None,
-    'conv': [tf.layers.conv1d, tf.layers.conv2d, tf.layers.conv3d],
-    'transposed_conv': [conv1d_transpose, tf.layers.conv2d_transpose, tf.layers.conv3d_transpose],
-    'separable_conv': [None, tf.layers.separable_conv2d, None],
-    'max_pooling': max_pooling,
-    'average_pooling': average_pooling,
-    'global_max_pooling': global_max_pooling,
-    'global_average_pooling': global_average_pooling,
-    'batch_norm': tf.layers.batch_normalization,
-    'dropout': tf.layers.dropout,
-    'mip': mip
+_CONV_LAYERS = {
+    1: tf.layers.conv1d,
+    2: tf.layers.conv2d,
+    3: tf.layers.conv3d
 }
 
-C_LAYERS = {
-    'a': 'activation',
-    'c': 'conv',
-    't': 'transposed_conv',
-    'p': 'max_pooling',
-    'v': 'average_pooling',
-    'P': 'global_max_pooling',
-    'V': 'global_average_pooling',
-    'n': 'batch_norm',
-    'd': 'dropout',
-    'm': 'mip'
-}
 
-_LAYERS_KEYS = str(list(C_LAYERS.keys()))
-_GROUP_KEYS = _LAYERS_KEYS.replace('v', 'p')
-C_GROUPS = dict(zip(_LAYERS_KEYS, _GROUP_KEYS))
-
-def _get_layer_fn(fn, dim):
-    f = ND_LAYERS[fn]
-    return f if callable(f) or f is None else f[dim-1]
-
-def _unpack_args(args, layer_no, layers_max):
-    new_args = {}
-    for arg in args:
-        if isinstance(args[arg], (tuple, list)) and layers_max > 1:
-            arg_value = args[arg][layer_no]
-        else:
-            arg_value = args[arg]
-        new_args.update({arg: arg_value})
-
-    return new_args
-
-def conv_block(dim, input_tensor, filters, kernel_size, layout='cnap', name=None,
-               strides=1, padding='same', data_format='channels_last', dilation_rate=1, depth_multiplier=1,
-               activation=tf.nn.relu, pool_size=2, pool_strides=2, dropout_rate=0., is_training=True, **kwargs):
-    """ Complex multi-dimensional convolution layer with batch normalization, activation, pooling and dropout
+def conv(dim, *args, **kwargs):
+    """ Nd convolution layer. Just a wrapper around ``tf.layers.conv1d``, ``conv2d``, ``conv3d``.
 
     Parameters
     ----------
-    d : int {1, 2, 3}
+    dim : int {1, 2, 3}
         number of dimensions
-    input_tensor : tf.Tensor
+
+    For more details see TensorFlow documentation:
+
+    - `tf.layers.conv1d <https://www.tensorflow.org/api_docs/python/tf/layers/conv1d>`_
+    - `tf.layers.conv2d <https://www.tensorflow.org/api_docs/python/tf/layers/conv2d>`_
+    - `tf.layers.conv3d <https://www.tensorflow.org/api_docs/python/tf/layers/conv3d>`_
+    """
+    layer_fn = _CONV_LAYERS[dim]
+    return layer_fn(*args, **kwargs)
+
+def conv1d_transpose(inputs, filters, kernel_size, strides=1, *args, **kwargs):
+    """ Transposed 1D convolution layer
+
+    Parameters
+    ----------
+    inputs : tf.Tensor
         input tensor
     filters : int
         number of filters in the ouput tensor
     kernel_size : int
         kernel size
-    layout : str
-        a sequence of layers:
-
-        - c - convolution (+ dilated, separable)
-        - t - transposed convolution
-        - n - batch normalization
-        - a - activation
-        - p - max pooling
-        - v - average pooling
-        - P - global max pooling
-        - V - global average pooling
-        - d - dropout
-        - m - maximum intensity projection (:func:`.layers.mip`)
-
-        Default is 'cnap'.
-    name : str
-        name of the layer that will be used as a scope.
     strides : int
-        Default is 1.
+        convolution stride. Default is 1.
+
+    Returns
+    -------
+    tf.Tensor
+    """
+    up_tensor = tf.expand_dims(inputs, 1)
+    conv_output = tf.layers.conv2d_transpose(up_tensor, filters=filters, kernel_size=(1, kernel_size),
+                                             strides=(1, strides), *args, **kwargs)
+    output = tf.squeeze(conv_output, [1])
+    return output
+
+def conv_transpose(dim, inputs, filters, kernel_size, strides=1, *args, **kwargs):
+    """ Transposed Nd convolution layer
+
+    Parameters
+    ----------
+    dim : int {1, 2, 3}
+        number of dimensions
+    inputs : tf.Tensor
+        input tensor
+    filters : int
+        number of filters in the ouput tensor
+    kernel_size : int
+        kernel size
+    strides : int
+        convolution stride. Default is 1.
+
+    Returns
+    -------
+    tf.Tensor
+    """
+    if dim == 1:
+        ouput = conv1d_transpose(inputs, filters, kernel_size, strides, *args, **kwargs)
+    elif dim == 2:
+        ouput = conv2d_transpose(inputs, filters, kernel_size, strides, *args, **kwargs)
+    elif dim == 3:
+        ouput = conv3d_transpose(inputs, filters, kernel_size, strides, *args, **kwargs)
+    return output
+
+
+def separable_conv(dim, inputs, filters, kernel_size, strides=1, padding='same', data_format='channels_last',
+                     dilation_rate=1, depth_multiplier=1, activation=None, name=None, *args, **kwargs):
+    """ Make Nd depthwise convolutions that acts separately on channels,
+        followed by a pointwise convolution that mixes channels.
+
+    Parameters
+    ----------
+    dim : int {1, 2, 3}
+        number of dimensions
+    inputs : tf.Tensor
+        input tensor
+    filters : int
+        number of filters in the ouput tensor
+    kernel_size : int
+        kernel size
+    strides : int
+        convolution stride. Default is 1.
     padding : str
         padding mode, can be 'same' or 'valid'. Default - 'same',
     data_format : str
         'channels_last' or 'channels_first'. Default - 'channels_last'.
-    dilation_rate: int
+    dilation_rate : int
         Default is 1.
+    depth_multiplier : int
+        The number of depthwise convolution output channels for each input channel.
+        The total number of depthwise convolution output channels will be equal to
+        ``num_filters_in`` * ``depth_multiplier``.
     activation : callable
         Default is `tf.nn.relu`.
-    pool_size : int
-        Default is 2.
-    pool_strides : int
-        Default is 2.
-    dropout_rate : float
-        Default is 0.
-    is_training : bool or tf.Tensor
-        Default is True.
-
-    conv : dict
-        parameters for convolution layers, like initializers, regularalizers, etc
-    transposed_conv : dict
-        parameters for transposed conv layers, like initializers, regularalizers, etc
-    batch_norm : dict
-        parameters for batch normalization layers, like momentum, intiializers, etc
-    max_pooling : dict
-        parameters for max_pooling layers, like initializers, regularalizers, etc
-    dropout : dict
-        parameters for dropout layers, like noise_shape, etc
+    name : str
+        The name of the layer
 
     Returns
     -------
-    output tensor : tf.Tensor
-
-    Notes
-    -----
-    When ``layout`` includes several layers of the same type, each one can have its own parameters,
-    if corresponding args are passed as lists/tuples.
-
-    Examples
-    --------
-    A simple 2d block: 3x3 conv, batch norm, relu, 2x2 max-pooling with stride 2::
-
-        x = conv_block(2, x, 32, 3, layout='cnap')
-
-    A canonical bottleneck block (1x1, 3x3, 1x1 conv with relu in-between)::
-
-        x = conv_block(2, x, [64, 64, 256], [1, 3, 1], layout='cacac')
-
-    A complex Nd block:
-
-    - 5x5 conv with 32 filters
-    - relu
-    - 3x3 conv with 32 filters
-    - relu
-    - 3x3 conv with 64 filters and a spatial stride 2
-    - relu
-    - batch norm
-    - dropout with rate 0.15
-
-    ::
-
-        x = conv_block(dim, x, [32, 32, 64], [5, 3, 3], layout='cacacand', strides=[1, 1, 2], dropout_rate=.15)
+    tf.Tensor
 
     """
+    conv_layer = _CONV_LAYERS[dim]
 
-    if not isinstance(dim, int) or dim < 1 or dim > 3:
-        raise ValueError("Number of dimensions should be 1, 2 or 3, but given %d" % dim)
+    if dim == 2:
+        return tf.layers.separable_conv2d(inputs, filters, kernel_size, strides, padding, data_format,
+                                          dilation_rate, depth_multiplier, activation, name, *args, **kwargs)
+    elif depth_multiplier == 1:
+        output = conv_layer(inputs, filters, kernel_size, strides, padding, data_format,
+                            dilation_rate, activation, name, *args, **kwargs)
+    else:
+        context = None
+        if name is not None:
+            context = tf.variable_scope(name)
+            context.__enter__()
 
-    context = None
-    if name is not None:
-        context = tf.variable_scope(name)
-        context.__enter__()
+        inputs_shape = inputs.get_shape().as_list()
+        axis = dim + 1 if data_format == 'channels_last' else 1
+        size = [-1] * (dim + 2)
+        size[axis] = 1
+        channels_in = inputs_shape[axis]
 
-    layout_dict = {}
-    for layer in layout:
-        if C_GROUPS[layer] not in layout_dict:
-            layout_dict[C_GROUPS[layer]] = [-1, 0]
-        layout_dict[C_GROUPS[layer]][1] += 1
+        depthwise_layers = []
+        for channel in range(channels_in):
+            start = [0] * (dim + 2)
+            start[axis] = channel
 
-    layout = layout.replace(' ', '')
-    tensor = input_tensor
-    for layer in layout:
+            print('--', channel, start, size, inputs_shape)
+            input_slice = tf.slice(inputs, start, size)
+            slice_conv = conv_layer(input_slice, depth_multiplier, kernel_size, strides, padding, data_format,
+                                    dilation_rate, activation, name='slice-%d' % channel, *args, **kwargs)
+            depthwise_layers.append(slice_conv)
 
-        layout_dict[C_GROUPS[layer]][0] += 1
-        layer_name = C_LAYERS[layer]
-        layer_fn = _get_layer_fn(layer_name, dim)
+        # Concatenate the per-channel convolutions along the channel dimension.
+        depthwise_conv = tf.concat(depthwise_layers, axis=axis)
 
-        if layer == 'a':
-            tensor = activation(tensor)
+        if channels_in * depth_multiplier != filters:
+            output = conv_layer(depthwise_conv, filters, 1, 1, padding, data_format, 1, activation,
+                                name='pointwise', *args, **kwargs)
         else:
-            if layer == 'c':
-                args = dict(filters=filters, kernel_size=kernel_size, strides=strides, padding=padding,
-                            data_format=data_format, dilation_rate=dilation_rate, depth_multiplier=depth_multiplier)
-            elif layer == 't':
-                args = dict(filters=filters, kernel_size=kernel_size, strides=strides, padding=padding,
-                            data_format=data_format)
-            elif layer == 'n':
-                args = dict(fused=True, axis=-1, training=is_training)
-            elif C_GROUPS[layer] == 'p':
-                args = dict(dim=dim, pool_size=pool_size, strides=pool_strides, padding=padding,
-                            data_format=data_format)
-            elif layer == 'd' and (not isinstance(dropout_rate, float) or dropout_rate > 0):
-                args = dict(rate=dropout_rate, training=is_training)
-            elif layer in ['P', 'V']:
-                args = dict(dim=dim, data_format=data_format)
-            elif layer == 'm':
-                args = {}
+            output = depthwise_conv
 
-            args = {**args, **kwargs.get(layer_name, {})}
-            args = _unpack_args(args, *layout_dict[C_GROUPS[layer]])
+        if context is not None:
+            context.__exit__(None, None, None)
 
-            if layer == 'c':
-                if args['depth_multiplier'] > 1:
-                    layer_fn = _get_layer_fn('separable_conv', dim)
-                else:
-                    args.pop('depth_multiplier')
-
-            tensor = layer_fn(inputs=tensor, **args)
-
-    if context is not None:
-        context.__exit__(None, None, None)
-
-    return tensor
+    return output
 
 
-def conv1d_block(input_tensor, filters, kernel_size, layout='cnap', name=None,
-                 strides=1, padding='same', data_format='channels_last', dilation_rate=1, depth_multiplier=1,
-                 activation=tf.nn.relu, pool_size=2, pool_strides=2, dropout_rate=0., is_training=True, **kwargs):
-    """ Complex 1d convolution with batch normalization, activation, pooling and dropout layers
+def separable_conv1d(inputs, filters, kernel_size, strides=1, padding='same', data_format='channels_last',
+                     dilation_rate=1, depth_multiplier=1, activation=None, name=None, *args, **kwargs):
+    """ Make 1d depthwise convolutions that acts separately on channels,
+        followed by a pointwise convolution that mixes channels.
 
-    See :func:`.conv_block` for details.
+    See :func:`.separable_conv` for details.
     """
-    return conv_block(1, input_tensor, filters, kernel_size, layout, name,
-                      strides, padding, data_format, dilation_rate, depth_multiplier, activation,
-                      pool_size, pool_strides, dropout_rate, is_training, **kwargs)
+    return separable_conv(1, inputs, filters, kernel_size, strides, padding, data_format,
+                          dilation_rate, depth_multiplier, activation, name, *args, **kwargs)
 
+def separable_conv2d(inputs, filters, kernel_size, strides=1, padding='same', data_format='channels_last',
+                     dilation_rate=1, depth_multiplier=1, activation=None, name=None, *args, **kwargs):
+    """ Make 2d depthwise convolutions that acts separately on channels,
+        followed by a pointwise convolution that mixes channels.
 
-def conv2d_block(input_tensor, filters, kernel_size, layout='cnap', name=None,
-                 strides=1, padding='same', data_format='channels_last', dilation_rate=1, depth_multiplier=1,
-                 activation=tf.nn.relu, pool_size=2, pool_strides=2, dropout_rate=0., is_training=True, **kwargs):
-    """ Complex 2d convolution with batch normalization, activation, pooling and dropout layers
+    See :func:`.separable_conv` for details.
 
-    See :func:`.conv_block` for details.
+    `tf.layers.separable_conv2d <https://www.tensorflow.org/api_docs/python/tf/layers/separable_conv2d>`_
+    is implemented using CUDA and, as a result, much faster.
     """
-    return conv_block(2, input_tensor, filters, kernel_size, layout, name,
-                      strides, padding, data_format, dilation_rate, depth_multiplier, activation,
-                      pool_size, pool_strides, dropout_rate, is_training, **kwargs)
+    return separable_conv(2, inputs, filters, kernel_size, strides, padding, data_format,
+                          dilation_rate, depth_multiplier, activation, name, *args, **kwargs)
 
+def separable_conv3d(inputs, filters, kernel_size, strides=1, padding='same', data_format='channels_last',
+                     dilation_rate=1, depth_multiplier=1, activation=None, name=None, *args, **kwargs):
+    """ Make 3d depthwise convolutions that acts separately on channels,
+        followed by a pointwise convolution that mixes channels.
 
-def conv3d_block(input_tensor, filters, kernel_size, layout='cnap', name=None,
-                 strides=1, padding='same', data_format='channels_last', dilation_rate=1, depth_multiplier=1,
-                 activation=tf.nn.relu, pool_size=2, pool_strides=2, dropout_rate=0., is_training=True, **kwargs):
-    """ Complex 2d convolution with batch normalization, activation, pooling and dropout layers
-
-    See :func:`.conv_block` for details.
+    See :func:`.separable_conv` for details.
     """
-    return conv_block(3, input_tensor, filters, kernel_size, layout, name,
-                      strides, padding, data_format, dilation_rate, depth_multiplier, activation,
-                      pool_size, pool_strides, dropout_rate, is_training, **kwargs)
+    return separable_conv(3, inputs, filters, kernel_size, strides, padding, data_format,
+                          dilation_rate, depth_multiplier, activation, name, *args, **kwargs)
