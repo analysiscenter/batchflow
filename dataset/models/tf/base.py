3118fc4cd4e88632d8d6bde8acda2d18287eded9
Roman Kh <rhudor@gmail.com>
2017-12-04 12:22:29 +0300

    Fix typo in  output tensor name

--- dataset/models/tf/base.py
+++ dataset/models/tf/base.py
@@ -1151,7 +1151,7 @@ class TFModel(BaseModel):
                 ctx = None
             attr_prefix = current_prefix + '_' if current_prefix else ''
 
-            self._add_output_predictions(x, scope, attr_prefix, **kwargs)
+            x = self._add_output_predictions(tensor, scope, attr_prefix, **kwargs)
             for oper in ops:
                 if oper == 'proba':
                     self._add_output_proba(x, scope, attr_prefix, **kwargs)
@@ -1167,6 +1167,7 @@ class TFModel(BaseModel):
         _ = scope, kwargs
         x = tf.identity(inputs, name='predictions')
         self.store_to_attr(attr_prefix + 'predictions', x)
+        return x
 
     def _add_output_proba(self, inputs, scope, attr_prefix, **kwargs):
         _ = scope, kwargs
