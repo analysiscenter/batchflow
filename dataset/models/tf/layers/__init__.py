307e54b9810a97020835a1ea5838a439a6e193e1
Roman Kh <rhudor@gmail.com>
2017-12-11 03:12:23 +0300

    Add resize with factor

--- dataset/models/tf/layers/__init__.py
+++ dataset/models/tf/layers/__init__.py
@@ -2,7 +2,7 @@
 import numpy as np
 import tensorflow as tf
 
-from .core import flatten, flatten2d, maxout, mip
+from .core import flatten, flatten2d, maxout, mip, xip
 from .conv_block import conv_block
-from .conv import conv1d_transpose, conv_transpose, separable_conv, separable_conv1d, separable_conv2d, separable_conv3d
+from .conv import conv1d_transpose, conv_transpose, separable_conv, subpixel_conv, resize_bilinear_additive
 from .pooling import max_pooling, average_pooling, global_average_pooling, global_max_pooling, fractional_max_pooling
