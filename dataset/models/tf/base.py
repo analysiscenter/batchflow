0c7de180240f3c84d0fd1c724747d97773001a8a
Roman Kh <rhudor@gmail.com>
2018-01-23 11:36:00 +0300

    Remove unneeded arg

--- dataset/models/tf/base.py
+++ dataset/models/tf/base.py
@@ -226,7 +226,7 @@ class TFModel(BaseModel):
         #. Create a tensorflow session
         """
 
-        def device_context(config):
+        def device_context():
             if 'device' in self.config:
                 device = self.config.get('device')
                 context = self.graph.device(device)
@@ -234,7 +234,7 @@ class TFModel(BaseModel):
                 context = contextlib.ExitStack()
             return context
 
-        with self.graph.as_default(), device_context(self.config):
+        with self.graph.as_default(), device_context():
             with tf.variable_scope(self.__class__.__name__):
                 with tf.variable_scope('globals'):
                     self.store_to_attr('is_training', tf.placeholder(tf.bool, name='is_training'))
