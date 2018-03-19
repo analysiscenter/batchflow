9976d292e2abd55b1f2ef1ae8fa866100c216f83
alexeykozhevin <a.kozhevin@analysiscenter.ru>
2018-03-19 11:11:33 +0300

    add additional logging

--- dataset/research/workers.py
+++ dataset/research/workers.py
@@ -87,6 +87,7 @@ class PipelineWorker(Worker):
                                     )
                                     item.save_results(filename, names=name)
                                 else:
+                                    self.log_info('Task {}, pipeline {}: {}'.format(i, name, j), filename=self.logfile)
                                     item.next_batch(name)
             except StopIteration:
                 self.log_info('Task {} was stopped after {} iterations'.format(i, j+1), filename=self.logfile)
