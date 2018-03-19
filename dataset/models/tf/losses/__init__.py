d19a2373b5ba25468b5f204f90a2d1c564d622ff
Roman Kh <rhudor@gmail.com>
2018-03-19 16:09:35 +0300

    Add dice2

--- dataset/models/tf/losses/__init__.py
+++ dataset/models/tf/losses/__init__.py
@@ -1,7 +1,28 @@
 """ Contains custom losses """
 import tensorflow as tf
 
-from ..layers import flatten
+
+def _dice(targets, predictions, weights=1.0, label_smoothing=0, scope=None,
+          loss_collection=tf.GraphKeys.LOSSES, reduction=tf.losses.Reduction.SUM_BY_NONZERO_WEIGHTS, _square=False):
+    e = 1e-6
+    predictions = tf.sigmoid(predictions)
+    axis = tuple(range(1, targets.shape.ndims))
+
+    if label_smoothing > 0:
+        num_classes = targets.shape[-1]
+        targets = targets * (1 - label_smoothing) + label_smoothing / num_classes
+
+    intersection = tf.reduce_sum(targets * predictions, axis=axis)
+    if _square:
+        targets = tf.reduce_sum(tf.square(targets), axis=axis)
+        predictions = tf.reduce_sum(tf.square(predictions), axis=axis)
+    else:
+        targets = tf.reduce_sum(targets, axis=axis)
+        predictions = tf.reduce_sum(predictions, axis=axis)
+
+    loss = -(2. * intersection + e) / (targets + predictions + e)
+    loss = tf.losses.compute_weighted_loss(loss, weights, scope, loss_collection, reduction)
+    return loss
 
 
 def dice(targets, predictions, weights=1.0, label_smoothing=0, scope=None,
@@ -21,18 +42,24 @@ def dice(targets, predictions, weights=1.0, label_smoothing=0, scope=None,
     Tensor of the same type as targets.
     If reduction is NONE, this has the same shape as targets; otherwise, it is scalar.
     """
-    e = 1e-6
-    predictions = tf.sigmoid(predictions)
-    axis = tuple(range(1, targets.shape.ndims))
+    return _dice(targets, predictions, weights, label_smoothing, scope, loss_collection, reduction)
 
-    if label_smoothing > 0:
-        num_classes = targets.shape[-1]
-        targets = targets * (1 - label_smoothing) + label_smoothing / num_classes
 
-    intersection = tf.reduce_sum(targets * predictions, axis=axis)
-    targets = tf.reduce_sum(targets, axis=axis)
-    predictions = tf.reduce_sum(predictions, axis=axis)
+def dice2(targets, predictions, weights=1.0, label_smoothing=0, scope=None,
+          loss_collection=tf.GraphKeys.LOSSES, reduction=tf.losses.Reduction.SUM_BY_NONZERO_WEIGHTS):
+    """ Dice coefficient with squares in denominator
 
-    loss = -(2. * intersection + e) / (targets + predictions + e)
-    loss = tf.losses.compute_weighted_loss(loss, weights, scope, loss_collection, reduction)
-    return loss
+    Parameters
+    ----------
+    targets : tf.Tensor
+        tensor with target values
+
+    predictions : tf.Tensor
+        tensor with predicted logits
+
+    Returns
+    -------
+    Tensor of the same type as targets.
+    If reduction is NONE, this has the same shape as targets; otherwise, it is scalar.
+    """
+    return _dice(targets, predictions, weights, label_smoothing, scope, loss_collection, reduction, _square=True)
