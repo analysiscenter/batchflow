c80e76f306e587625603a92c2f6deadca8afad15
Roman Kh <rhudor@gmail.com>
2018-05-08 04:50:06 +0300

    Update best_practice options management

--- dataset/best_practice.py
+++ dataset/best_practice.py
@@ -1,4 +1,4 @@
 """ Enables model defaults for faster training and more accurate preductions """
-from . import enable_best_practice
+from . import setup_best_practice
 
-enable_best_practice()
+setup_best_practice(enable='all')
