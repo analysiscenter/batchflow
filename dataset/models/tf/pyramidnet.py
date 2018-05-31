27ec874114adf3b2986bd203faf1a9d62f51b4a1
Roman Kh <rhudor@gmail.com>
2018-05-31 13:01:26 +0300

    Add PyramidNet

---
+++ dataset/models/tf/pyramidnet.py
@@ -0,0 +1,71 @@
+"""
+Dongyoon Han et al. "`Deep Pyramidal Residual Networks
+<https://arxiv.org/abs/1610.02915>`_"
+
+"""
+import numpy as np
+import tensorflow as tf
+
+from ... import is_best_practice
+from . import TFModel, ResNet
+from .layers import conv_block
+
+
+class PyramidNet(ResNet):
+    """ The base PyramidNet model
+
+    **Configuration**
+
+    inputs : dict
+        dict with 'images' and 'labels' (see :meth:`._make_inputs`)
+
+    input_block : dict
+        parameters for the input block (see :func:`.conv_block`).
+
+    body : dict
+        num_blocks : list of int
+            number of blocks in each group with the same number of filters.
+
+        block : dict
+            widening : int
+                an increment of filters number in each block (default=8)
+
+            and other :class:`~.ResNet` block params
+
+    head : dict
+        'Vdf' with dropout_rate=.4
+
+    Notes
+    -----
+    Also see :class:`~.TFModel` and :class:`~.ResNet` configuration.
+    """
+    @classmethod
+    def default_config(cls):
+        config = ResNet.default_config()
+        config['body/block/widening'] = 8
+        config['body/block/zero_pad'] = True
+        return config
+
+    @classmethod
+    def default_layout(cls, bottleneck, **kwargs):
+        """ Define conv block layout """
+        return 'nc nac nac n' if bottleneck else 'nc nac n'
+
+    def build_config(self, names=None):
+        config = super(ResNet, self).build_config(names)
+
+        if config.get('body/filters') is None:
+            w = config['body/block/widening']
+            filters = config['input_block/filters']
+            config['body/filters'] = []
+            for g in config['body/num_blocks']:
+                bfilters = [filters +  w * b for b in range(1, g + 1)]
+                filters = bfilters[-1]
+                config['body/filters'].append(bfilters)
+
+        if config.get('head/units') is None:
+            config['head/units'] = self.num_classes('targets')
+        if config.get('head/filters') is None:
+            config['head/filters'] = self.num_classes('targets')
+
+        return config
--- dataset/models/tf/resnet.py
+++ dataset/models/tf/resnet.py
@@ -42,8 +42,11 @@ class ResNet(TFModel):
                 whether to use bottleneck blocks (1x1,3x3,1x1) or simple (3x3,3x3)
             bottleneck_factor : int
                 filter shrinking factor in a bottleneck block (default=4)
-            post_activation : None or callable
-                an activation to use after residual and shortcut summation (default is None)
+            post_activation : None or bool or str
+                layout to apply after after residual and shortcut summation (default is None)
+            zero_pad : bool
+                whether to pad a shortcut with zeros when a number of filters increases
+                or apply a 1x1 convolution (default is False)
             width_factor : int
                 widening factor to make WideResNet (default=1)
             se_block : bool
@@ -65,9 +68,9 @@ class ResNet(TFModel):
         config['input_block'].update(dict(layout='cnap', filters=64, kernel_size=7, strides=2,
                                           pool_size=3, pool_strides=2))
 
-        config['body/block'] = dict(post_activation=None, downsample=False,
+        config['body/block'] = dict(layout=None, post_activation=None, downsample=False,
                                     bottleneck=False, bottleneck_factor=4,
-                                    width_factor=1,
+                                    width_factor=1, zero_pad=False,
                                     resnext=False, resnext_factor=32,
                                     se_block=False, se_factor=16)
 
@@ -84,13 +87,22 @@ class ResNet(TFModel):
             config['optimizer'] = ('Momentum', dict(momentum=.9))
         return config
 
+    @classmethod
+    def default_layout(cls, bottleneck, **kwargs):
+        """ Define conv block layout """
+        reps = 3 if bottleneck else 2
+        return 'cna' * reps if is_best_practice() else 'nac' * reps
+
     def build_config(self, names=None):
+        """ Define a model architecture configuration """
         config = super().build_config(names)
 
         if config.get('body/filters') is None:
+            width = config['body/block/width_factor']
             num_blocks = config['body/num_blocks']
             filters = config['input_block/filters']
-            config['body/filters'] = 2 ** np.arange(len(num_blocks)) * filters * config['body/block/width_factor']
+            config['body/filters'] = (2 ** np.arange(len(num_blocks)) * filters * width).tolist()
+
         if config.get('head/units') is None:
             config['head/units'] = self.num_classes('targets')
         if config.get('head/filters') is None:
@@ -106,7 +118,7 @@ class ResNet(TFModel):
         ----------
         inputs : tf.Tensor
             input tensor
-        filters : list of int
+        filters : list of int or list of list of int
             number of filters in each block group
         num_blocks : list of int
             number of blocks in each group
@@ -132,7 +144,8 @@ class ResNet(TFModel):
                     for block in range(n_blocks):
                         downsample = i > 0 and block == 0
                         block_args['downsample'] = downsample
-                        x = cls.block(x, filters=filters[i], name='block-%d' % block, **block_args)
+                        bfilters = filters[i] if isinstance(filters[i], int) else filters[i][block]
+                        x = cls.block(x, filters=bfilters, name='block-%d' % block, **block_args)
                     x = tf.identity(x, name='output')
         return x
 
@@ -168,8 +181,12 @@ class ResNet(TFModel):
         ----------
         inputs : tf.Tensor
             input tensor
+        layout : str
+            a sequence of layers in the block
         filters : int or list/tuple of ints
             number of output filters
+        zero_pad : bool
+            whether to pad shortcut when a number of filters increases
         width_factor : int
             a factor to increase number of filters to build a wide network
         downsample : bool
@@ -196,53 +213,68 @@ class ResNet(TFModel):
         tf.Tensor
         """
         kwargs = cls.fill_params('body/block', **kwargs)
-        filters, downsample = cls.pop(['filters', 'downsample'], kwargs)
+        layout, filters, downsample, zero_pad = cls.pop(['layout', 'filters', 'downsample', 'zero_pad'], kwargs)
         width_factor = cls.pop('width_factor', kwargs)
         bottleneck, bottleneck_factor = cls.pop(['bottleneck', 'bottleneck_factor'], kwargs)
         resnext, resnext_factor = cls.pop(['resnext', 'resnext_factor'], kwargs)
         se_block, se_factor = cls.pop(['se_block', 'se_factor'], kwargs)
         post_activation = cls.pop('post_activation', kwargs)
+        if isinstance(post_activation, bool) and post_activation:
+            post_activation = 'an'
 
         with tf.variable_scope(name):
             filters = filters * width_factor
             if resnext:
-                x = cls.next_sub_block(inputs, filters, bottleneck, resnext_factor, name='sub',
-                                       downsample=downsample, **kwargs)
+                x = cls.next_conv_block(inputs, layout, filters, bottleneck, resnext_factor, name='conv',
+                                        downsample=downsample, **kwargs)
             else:
-                x = cls.sub_block(inputs, filters, bottleneck, bottleneck_factor, name='sub',
-                                  downsample=downsample, **kwargs)
+                x = cls.conv_block(inputs, layout, filters, bottleneck, bottleneck_factor, name='conv',
+                                   downsample=downsample, **kwargs)
 
             data_format = kwargs.get('data_format')
             inputs_channels = cls.num_channels(inputs, data_format)
             x_channels = cls.num_channels(x, data_format)
 
-            if inputs_channels != x_channels or downsample:
-                strides = 2 if downsample else 1
-                shortcut = conv_block(inputs, name='shortcut', **{**kwargs, **dict(layout='c', filters=x_channels,
-                                                                                   kernel_size=1, strides=strides)})
-            else:
-                shortcut = inputs
-
             if se_block:
                 x = cls.se_block(x, se_factor, **kwargs)
+
+            with tf.variable_scope('shortcut'):
+                strides = 2 if downsample else 1
+                shortcut = inputs
+                if zero_pad and inputs_channels < x_channels:
+                    if downsample:
+                        shortcut = conv_block(shortcut, name='shortcut',
+                                              **{**kwargs, **dict(layout='c', filters=inputs_channels,
+                                                                  kernel_size=1, strides=strides)})
+                    if inputs_channels < x_channels:
+                        axis = cls.channels_axis(kwargs.get('data_format'))
+                        padding = [[0, 0] for _ in range(shortcut.shape.ndims)]
+                        padding[axis] = [0, x_channels - inputs_channels]
+                        shortcut = tf.pad(shortcut, padding)
+                elif inputs_channels != x_channels or downsample:
+                    shortcut = conv_block(shortcut, name='shortcut',
+                                          **{**kwargs, **dict(layout='c', filters=x_channels,
+                                                              kernel_size=1, strides=strides)})
 
             x = x + shortcut
 
             if post_activation:
-                x = post_activation(x)
+                x = conv_block(x, layout=post_activation, name='post_activation', **kwargs)
 
             x = tf.identity(x, name='output')
 
         return x
 
     @classmethod
-    def sub_block(cls, inputs, filters, bottleneck, bottleneck_factor, **kwargs):
+    def conv_block(cls, inputs, layout, filters, bottleneck, bottleneck_factor, **kwargs):
         """ ResNet convolution block
 
         Parameters
         ----------
         inputs : tf.Tensor
             input tensor
+        layout : str
+            a sequence of layers in the block
         filters : int
             number of output filters
         downsample : bool
@@ -258,20 +290,24 @@ class ResNet(TFModel):
         -------
         tf.Tensor
         """
+        if layout is None:
+            layout = cls.default_layout(bottleneck=bottleneck, filters=filters, **kwargs)
         if bottleneck:
-            x = cls.bottleneck_block(inputs, filters=filters, bottleneck_factor=bottleneck_factor, **kwargs)
+            x = cls.bottleneck_block(inputs, layout, filters, bottleneck_factor=bottleneck_factor, **kwargs)
         else:
-            x = cls.simple_block(inputs, filters=filters, **kwargs)
+            x = cls.simple_block(inputs, layout, filters, **kwargs)
         return x
 
     @classmethod
     def simple_block(cls, inputs, layout=None, filters=None, kernel_size=3, downsample=False, **kwargs):
-        """ A simple residual block with two 3x3 convolutions
+        """ A simple residual block (by default with two 3x3 convolutions)
 
         Parameters
         ----------
         inputs : tf.Tensor
             input tensor
+        layout : str
+            a sequence of layers in the block
         filters : int
             number of filters
         kernel_size : int or tuple
@@ -287,8 +323,6 @@ class ResNet(TFModel):
         -------
         tf.Tensor
         """
-        if layout is None:
-            layout = 'cna cna' if is_best_practice() else 'nac nac'
         n = layout.count('c') + layout.count('C') - 1
         strides = ([2] + [1] * n) if downsample else 1
         return conv_block(inputs, layout, filters=filters, kernel_size=kernel_size, strides=strides, **kwargs)
@@ -302,6 +336,8 @@ class ResNet(TFModel):
         ----------
         inputs : tf.Tensor
             input tensor
+        layout : str
+            a sequence of layers in the block
         filters : int
             number of filters in the first two convolutions
         kernel_size : int or tuple
@@ -318,8 +354,6 @@ class ResNet(TFModel):
         tf.Tensor
         """
         kernel_size = [1, 3, 1] if kernel_size is None else kernel_size
-        if layout is None:
-            layout = 'cna cna cna' if is_best_practice() else 'nac nac nac'
         n = layout.count('c') + layout.count('C') - 2
         if kwargs.get('strides') is None:
             strides = ([1, 2] + [1] * n) if downsample else 1
@@ -330,7 +364,7 @@ class ResNet(TFModel):
         return x
 
     @classmethod
-    def next_sub_block(cls, inputs, filters, bottleneck, resnext_factor, name, **kwargs):
+    def next_conv_block(cls, inputs, filters, bottleneck, resnext_factor, name, **kwargs):
         """ ResNeXt convolution block
 
         Parameters
@@ -346,7 +380,7 @@ class ResNet(TFModel):
         name : str
             scope name
         kwargs : dict
-            `sub_block` parameters
+            `conv_block` parameters
 
         Returns
         -------
@@ -356,8 +390,8 @@ class ResNet(TFModel):
         sub_blocks = []
         with tf.variable_scope(name):
             for i in range(resnext_factor):
-                x = cls.sub_block(inputs, filters=_filters, bottleneck=bottleneck, bottleneck_factor=filters//4,
-                                  name='next_sub_block-%d' % i, **kwargs)
+                x = cls.conv_block(inputs, filters=_filters, bottleneck=bottleneck, bottleneck_factor=filters//4,
+                                   name='next_conv_block-%d' % i, **kwargs)
                 sub_blocks.append(x)
             x = tf.add_n(sub_blocks)
         return x
