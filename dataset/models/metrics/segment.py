94125a0a6627d2a901cf12f83e5097176bab6a02
Roman Kh <rhudor@gmail.com>
2018-06-21 17:14:01 +0300

    Add components segmentation metrics

--- dataset/models/metrics/segment.py
+++ dataset/models/metrics/segment.py
@@ -1,5 +1,23 @@
 """ Contains metrics for segmentation """
-from . import ClassificationMetrics
+from ... import parallel
+from . import ClassificationMetrics, get_components
 
-class SegmentationMetrics(ClassificationMetrics):
+
+class SegmentationMetricsByPixels(ClassificationMetrics):
+    """ Metrics to assess segmentation models pixel-wise """
     pass
+
+
+class SegmentationMetricsByComponents(ClassificationMetrics):
+    """ Metrics to assess segmentation models with connected components """
+    def __init__(self, targets, predictions, fmt='proba', num_classes=None, axis=None, threshold=.5, iot=.5):
+        super().__init__(targets, predictions, fmt, num_classes, axis, threshold, confusion=False)
+        self.iot = iot
+        self.target_components = get_components(self.targets, batch=True)
+        self.predicted_components = get_components(self.predictions, batch=True)
+        self._calc_confusion()
+
+    def _calc_confusion(self):
+        for i in range(len(self.target_components)):
+            for j in range(len(self.target_components[i])):
+                pass
