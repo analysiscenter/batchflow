177f465a2a0ab8799ba267fd3a5bce8a5b1385c9
Roman Kh <rhudor@gmail.com>
2018-06-22 17:30:01 +0300

    Add multiclass segmentation

--- dataset/models/metrics/segment.py
+++ dataset/models/metrics/segment.py
@@ -14,20 +14,29 @@ class SegmentationMetricsByInstances(ClassificationMetrics):
     def __init__(self, targets, predictions, fmt='proba', num_classes=None, axis=None, threshold=.5, iot=.5):
         super().__init__(targets, predictions, fmt, num_classes, axis, threshold, confusion=False)
         self.iot = iot
-        self.target_instances = self._get_instances(self.targets)
-        self.predicted_instances = self._get_instances(self.predictions)
+        self.target_instances = self._get_instances(self.targets, axis)
+        self.predicted_instances = self._get_instances(self.predictions, axis)
         self._calc_confusion()
 
-    def _get_instances(self, inputs):
-        return get_components(inputs, batch=True)
+    def _get_instances(self, inputs, axis):
+        if axis is None:
+            instances = [get_components(inputs, batch=True)]
+        else:
+            instances = []
+            shape = [slice(None)] * inputs.ndim
+            for i in range(1, inputs.shape[axis]):
+                shape[axis] = i
+                per_class = get_components(inputs[shape], batch=True)
+                instances.append(per_class)
+        return instances
 
     def _calc_confusion(self):
         self._confusion_matrix = np.zeros((self.targets.shape[0], self.num_classes - 1, 2, 2), dtype=np.intp)
 
-        for i in range(len(self.target_instances)):
-            for j in range(len(self.target_instances[i])):
-                for k in range(self.num_classes - 1):
-                    c = self.target_instances[i][j]
+        for k in range(self.num_classes - 1):
+            for i in range(len(self.target_instances[k])):
+                for j in range(len(self.target_instances[k][i])):
+                    c = self.target_instances[k][i][j]
                     coords = [slice(i, i+1)]
                     for d in range(c.shape[0]):
                         coords.append(slice(c[d, 0], c[d, 1]))
@@ -38,10 +47,10 @@ class SegmentationMetricsByInstances(ClassificationMetrics):
                     else:
                         self._confusion_matrix[i, k, 0, 1] += 1
 
-        for i in range(len(self.predicted_instances)):
-            for j in range(len(self.predicted_instances[i])):
-                for k in range(self.num_classes - 1):
-                    c = self.predicted_instances[i][j]
+        for k in range(self.num_classes - 1):
+            for i in range(len(self.predicted_instances[k])):
+                for j in range(len(self.predicted_instances[k][i])):
+                    c = self.predicted_instances[k][i][j]
                     coords = [slice(i, i+1)]
                     for d in range(c.shape[0]):
                         coords.append(slice(c[d, 0], c[d, 1]))
