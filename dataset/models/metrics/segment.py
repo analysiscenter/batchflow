5b52ef33c9faf32d9b49cfdcf453da7646f7edd8
Roman Kh <rhudor@gmail.com>
2018-06-22 11:40:19 +0300

    Calc full confusion matrix for components segmentation

--- dataset/models/metrics/segment.py
+++ dataset/models/metrics/segment.py
@@ -20,12 +20,10 @@ class SegmentationMetricsByComponents(ClassificationMetrics):
         self.predicted_components = get_components(self.predictions, batch=True)
         t = time()
         self._calc_confusion()
-        print(time() - t)
+        print('matrix', time() - t)
 
     def _calc_confusion(self):
         self._confusion_matrix = np.zeros((self.targets.shape[0], self.num_classes - 1, 2, 2), dtype=np.int32)
-        print(self.target_components)
-        print(self.predicted_components)
 
         for i in range(len(self.target_components)):
             for j in range(len(self.target_components[i])):
@@ -34,7 +32,21 @@ class SegmentationMetricsByComponents(ClassificationMetrics):
                     coords = [slice(i, i+1)]
                     for d in range(c.shape[0]):
                         coords.append(slice(c[d, 0], c[d, 1]))
-                    print(coords)
-                    t = self.targets[coords]
-                    p = self.predictions[coords]
-                    #self._confusion_matrix[i, k, 1, 1] += 1
+                    targ = self.targets[coords]
+                    pred = self.targets[coords] * self.predictions[coords]
+                    if np.sum(pred) / targ.size >= self.iot:
+                        self._confusion_matrix[i, k, 1, 1] += 1
+                    else:
+                        self._confusion_matrix[i, k, 0, 1] += 1
+
+        for i in range(len(self.predicted_components)):
+            for j in range(len(self.predicted_components[i])):
+                for k in range(self.num_classes - 1):
+                    c = self.predicted_components[i][j]
+                    coords = [slice(i, i+1)]
+                    for d in range(c.shape[0]):
+                        coords.append(slice(c[d, 0], c[d, 1]))
+                    pred = self.predictions[coords]
+                    targ = self.targets[coords]
+                    if np.sum(pred) / targ.size < self.iot:
+                        self._confusion_matrix[i, k, 1, 0] += 1
