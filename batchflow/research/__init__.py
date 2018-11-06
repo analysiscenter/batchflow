da31787866cb4201e72b29570c544db54c617c14
alexeykozhevin <a.kozhevin@analysiscenter.ru>
2018-11-06 23:53:46 +0300

    add load_results

--- batchflow/research/__init__.py
+++ batchflow/research/__init__.py
@@ -2,5 +2,4 @@
 from .grid import KV, Grid, Option, ConfigAlias
 from .distributor import Worker, Distributor
 from .workers import PipelineWorker
-from .research import Research
-from .results import Results
+from .research import Research, Results
