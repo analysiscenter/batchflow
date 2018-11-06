76ec916d2dc4c1100e007e5b8c96887c1752be4f
alexeykozhevin <a.kozhevin@analysiscenter.ru>
2018-11-07 01:39:04 +0300

    remove #

--- batchflow/pipeline.py
+++ batchflow/pipeline.py
@@ -1169,7 +1169,7 @@ class Pipeline:
             batch_generator = self.gen_rebatch(*args, **kwargs, prefetch=prefetch)
             prefetch = 0
         else:
-            #self.reset_iter(exclude_dataset=True)
+            self.reset_iter(exclude_dataset=True)
             batch_generator = self.dataset.gen_batch(*args, **kwargs)
 
         if prefetch > 0:
