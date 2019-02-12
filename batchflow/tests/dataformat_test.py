8872c56f6d8f54337d6df856f02bcf1779fd7fc1
Sergey Tsimfer <Tsimfer.SA@gazprom-neft.ru>
2019-02-12 17:25:46 +0300

    added init

--- batchflow/tests/dataformat_test.py
+++ batchflow/tests/dataformat_test.py
@@ -11,129 +11,11 @@ import pytest
 import numpy as np
 import tensorflow as tf
 
-sys.path.append('..')
-from my_base import TFModel
-# from batchflow.models.tf.base import TFModel
+from ..models.tf.base import TFModel
 from batchflow import Pipeline, ImagesBatch, Dataset
 from batchflow import B, V
 from batchflow.models.tf.layers import conv_block
-# from batchflow.models.tf import VGG7
-
-
-_VGG7_ARCH = [
-    (2, 0, 64, 1),
-    (2, 0, 128, 1),
-    (2, 1, 256, 1)
-]
-
-
-class VGG(TFModel):
-    """ Base VGG neural network
-
-    **Configuration**
-
-    inputs : dict
-        dict with keys 'images' and 'labels' (see :meth:`~.TFModel._make_inputs`)
-
-    body/arch : list of tuple of int
-        Each list item contains parameters for one network block as a tuple of 4 ints:
-
-        - number of convolution layers with 3x3 kernel
-        - number of convolution layers with 1x1 kernel
-        - number of filters in each layer
-        - whether to downscale the image at the end of the block with max_pooling (2x2, stride=2)
-
-    body/block : dict
-        :func:`.conv_block` parameters
-    """
-    @classmethod
-    def default_config(cls):
-        config = TFModel.default_config()
-        config['common/conv/use_bias'] = False
-        config['body/block'] = dict(layout='cna', pool_size=2, pool_strides=2)
-        config['head'] = dict(layout='Vdf', dropout_rate=.8, units=2)
-
-        config['loss'] = 'ce'
-        config['decay'] = ('const', dict(boundaries=[92500, 185000, 277500], values=[1e-2, 1e-3, 1e-4, 1e-5]))
-        config['optimizer'] = ('Momentum', dict(momentum=.9))
-        return config
-
-    def build_config(self, names=None):
-        config = super().build_config(names)
-        if isinstance(config['head/units'], list):
-            config['head/units'][-1] = self.num_classes('targets')
-        else:
-            config['head/units'] = self.num_classes('targets')
-        return config
-
-    @classmethod
-    def body(cls, inputs, name='body', **kwargs):
-        """ Create base VGG layers
-
-        Parameters
-        ----------
-        inputs : tf.Tensor
-            input tensor
-        arch : list of tuples
-            number of 3x3 conv, number of 1x1 conv, number of filters, whether to downscale
-        name : str
-            scope name
-
-        Returns
-        -------
-        tf.Tensor
-        """
-        kwargs = cls.fill_params('body', **kwargs)
-        arch = kwargs.pop('arch')
-        if not isinstance(arch, (list, tuple)):
-            raise TypeError("arch must be list or tuple, but {} was given.".format(type(arch)))
-        block = kwargs.pop('block')
-        block = {**block, **kwargs}
-
-        x = inputs
-        with tf.variable_scope(name):
-            for i, block_cfg in enumerate(arch):
-                x = cls.block(x, *block_cfg, name='block-%d' % i, **block)
-        return x
-
-    @classmethod
-    def block(cls, inputs, depth3, depth1, filters, downscale, name='block', **kwargs):
-        """ A sequence of 3x3 and 1x1 convolutions followed by pooling
-
-        Parameters
-        ----------
-        inputs : tf.Tensor
-            input tensor
-        depth3 : int
-            the number of convolution layers with 3x3 kernel
-        depth1 : int
-            the number of convolution layers with 1x1 kernel
-        filters : int
-            the number of filters in each convolution layer
-        downscale : bool
-            whether to decrease spatial dimension at the end of the block
-
-        Returns
-        -------
-        tf.Tensor
-        """
-        kwargs = cls.fill_params('body/block', **kwargs)
-        layout = kwargs.pop('layout') * (depth3 + depth1) + 'p' * downscale
-        kernels = [3] * depth3 + [1] * depth1
-        with tf.variable_scope(name):
-            x = conv_block(inputs, layout, filters, kernels, name='conv', **kwargs)
-            x = tf.identity(x, name='output')
-        return x
-
-class VGG7(VGG):
-    """ VGG7 network """
-    @classmethod
-    def default_config(cls):
-        config = VGG.default_config()
-        config['body/arch'] = _VGG7_ARCH
-        return config
-
-
+from ..models.tf.vgg import VGG7
 
 class SingleModel(TFModel):
     pass
@@ -219,7 +101,6 @@ def get_single_pipeline():
                          )
         return test_pipeline
     return _get_single_pipeline
-
 
 
 class MultiBatch(ImagesBatch):
@@ -383,7 +264,7 @@ def setup(single_setup, multi_setup, model_noinitial_setup):
             return single_setup(d_f)
         if mode == 'multi_input':
             return multi_setup(d_f)
-        if mode in ['vgg', 'resnet']:
+        if mode in ['vgg']:
             return model_noinitial_setup(d_f)
 
     return _setup
