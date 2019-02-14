4f07b0895b390c6ba3247ed4261d39115c6b16a9
Sergey Tsimfer <Tsimfer.SA@gazprom-neft.ru>
2019-02-14 12:40:09 +0300

    clean up

--- batchflow/tests/conftest.py
+++ batchflow/tests/conftest.py
@@ -1,4 +1,5 @@
 """ Pytest configuration. """
 # pylint: disable=invalid-name, unused-import
+# pylint: disable=unused-wildcard-import, wildcard-import
 import pytest
 from .fixtures.dataformat import *
--- conftest.py
+++
@@ -1,5 +0,0 @@
-""" Pytest configuration. """
-# pylint: disable=invalid-name, unused-import
-import pytest
-
-pytest_plugins = ["batchflow.tests.fixtures.dataformat",]
