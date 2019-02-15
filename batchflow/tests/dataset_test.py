0c9ee6fd4a8a74b838a0a6d1035d0844242ba570
pphator <ppahtor@gmail.com>
2019-02-15 11:29:17 +0300

    Fix mistakes in Dataset unit tests
    Also disable pylint docstring checking

--- batchflow/tests/dataset_test.py
+++ batchflow/tests/dataset_test.py
@@ -1,21 +1,15 @@
-import sys
+#pylint: disable=missing-docstring”
+
 import pytest
 import numpy as np
 
-
-from batchflow import Dataset
-
-from batchflow import Baseset
-from batchflow import Batch
-from batchflow import DatasetIndex
-from batchflow import Pipeline
+from batchflow import Dataset, Batch, DatasetIndex, Pipeline
 
 
 @pytest.fixture
 def dataset():
     index = DatasetIndex(np.arange(100))
-    batch_class = Batch
-    return Dataset(index, batch_class)
+    return Dataset(index, Batch)
 
 
 class TestDataset:
@@ -25,10 +19,10 @@ class TestDataset:
         assert isinstance(new_ds, Dataset)
         assert (new_ds.index.index == new_index.index).all()
 
-    def test_build_index(self, dataset):
+    def test_build_index(self):
         new_index = Dataset.build_index(np.arange(25))
         assert isinstance(new_index, DatasetIndex)
-        assert len(new_index.index) > 0
+        assert len(new_index.index) == 25
 
     def test_create_subset(self, dataset):
         new_index = DatasetIndex(np.arange(25))
@@ -38,7 +32,7 @@ class TestDataset:
 
     def test_create_batch(self, dataset):
         target_index = DatasetIndex(np.arange(5))
-        new_batch = dataset.create_batch(target_index)
+        new_batch = dataset.create_batch(target_index, pos=True)
         assert isinstance(new_batch, dataset.batch_class)
         assert len(new_batch.index.index) == len(target_index.index)
 
