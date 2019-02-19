490ae28fa71b8185c5cf4d5e84d84fb2869e528f
Sergey Tsimfer <Tsimfer.SA@gazprom-neft.ru>
2019-02-19 14:51:01 +0300

    fixes

--- batchflow/tests/config_pass_test.py
+++ batchflow/tests/config_pass_test.py
@@ -1,13 +1,10 @@
 """ Test for passing information through config. """
-# pylint: disable=import-error, wrong-import-position, no-name-in-module
+# pylint: disable=import-error, no-name-in-module
 # pylint: disable=missing-docstring, redefined-outer-name
-import os
-import warnings
-warnings.filterwarnings("ignore")
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 import pytest
 import tensorflow as tf
+
+from batchflow import Config
 from batchflow.models.tf import TFModel
 
 
@@ -21,7 +18,7 @@ def single_config():
     """ Fixture that returns the simplest config for single-input model. """
 
     class SingleModel(TFModel):
-        test_container = {}
+        model_args = Config()
 
         @classmethod
         def default_config(cls):
@@ -32,13 +29,13 @@ def single_config():
         @classmethod
         def initial_block(cls, inputs, name='initial_block', **kwargs):
             kwargs = cls.fill_params(name, **kwargs)
-            cls.test_container['test_initial_block'] = kwargs
+            cls.model_args['initial_block'] = kwargs
             return inputs
 
         @classmethod
         def body(cls, inputs, name='body', **kwargs):
             kwargs = cls.fill_params(name, **kwargs)
-            cls.test_container['test_body'] = kwargs
+            cls.model_args['body'] = kwargs
 
             block_args = cls.pop('block', kwargs)
             block_args = {**kwargs, **block_args}
@@ -48,14 +45,14 @@ def single_config():
         @classmethod
         def block(cls, inputs, **kwargs):
             kwargs = cls.fill_params('body/block', **kwargs)
-            cls.test_container['test_block'] = kwargs
+            cls.model_args['block'] = kwargs
             return inputs
 
         @classmethod
         def head(cls, inputs, name='head', **kwargs):
             inputs = super().head(inputs, **kwargs)
             kwargs = cls.fill_params(name, **kwargs)
-            cls.test_container['test_head'] = kwargs
+            cls.model_args['head'] = kwargs
             return inputs
 
     config = {'inputs': {'images': {'shape': (10, 10, 3)},
@@ -69,10 +66,10 @@ def single_config():
 
 @pytest.fixture()
 def multi_config():
-    """ Fixture that returnst the simplest config for multi-input model. """
+    """ Fixture that returns the simplest config for multi-input model. """
 
     class MultiModel(TFModel):
-        test_container = {}
+        model_args = Config()
 
         @classmethod
         def default_config(cls):
@@ -84,42 +81,42 @@ def multi_config():
         @classmethod
         def initial_block(cls, inputs, name='initial_block', **kwargs):
             kwargs = cls.fill_params(name, **kwargs)
-            cls.test_container['test_initial_block'] = kwargs
+            cls.model_args['initial_block'] = kwargs
             return inputs
 
         @classmethod
         def body(cls, inputs, name='body', **kwargs):
             kwargs = cls.fill_params(name, **kwargs)
-            cls.test_container['test_body'] = kwargs
+            cls.model_args['body'] = kwargs
 
             block_args = cls.pop('block', kwargs)
             block_args = {**kwargs, **block_args}
             branch_args = cls.pop('branch', kwargs)
             branch_args = {**kwargs, **branch_args}
-            with tf.variable_scope(name):
-                input_1, input_2 = inputs
-                x_1 = cls.block(input_1, **block_args)
-                x_2 = cls.branch(input_2, **branch_args)
-                output = tf.add(x_1, x_2)
+
+            input_1, input_2 = inputs
+            x_1 = cls.block(input_1, **block_args)
+            x_2 = cls.branch(input_2, **branch_args)
+            output = tf.add(x_1, x_2)
             return output
 
         @classmethod
         def block(cls, input_1, **kwargs):
             kwargs = cls.fill_params('body/block', **kwargs)
-            cls.test_container['test_block'] = kwargs
+            cls.model_args['block'] = kwargs
             return input_1
 
         @classmethod
         def branch(cls, input_2, **kwargs):
             kwargs = cls.fill_params('body/branch', **kwargs)
-            cls.test_container['test_branch'] = kwargs
+            cls.model_args['branch'] = kwargs
             return input_2
 
         @classmethod
         def head(cls, inputs, name='head', **kwargs):
             inputs = super().head(inputs, name='head', **kwargs)
             kwargs = cls.fill_params(name, **kwargs)
-            cls.test_container['test_head'] = kwargs
+            cls.model_args['head'] = kwargs
             return inputs
 
     config = {'inputs': {'images_1': {'shape': (10, 10, 3)},
@@ -151,61 +148,61 @@ class Test_config_pass():
         Then 'config' is modified by adding or changing some of the keys and values. Usually,
         modifications are done only at part, specified by 'location' parameter.
         At last, we check that our modifications are actually in place by observing
-        contents of 'test_container'.
+        contents of 'model_args'.
         """
     @pytest.mark.parametrize('location', LOCATIONS)
     def test_common(self, location, model_and_config):
         """ Easiest way to pass a key to all of the parts of the model is to
         pass it to 'common'.
         """
-        model, config = model_and_config
+        model_class, config = model_and_config
         config['common/common_key'] = 'common_key_modified'
-        container = model(config).test_container
-        assert container['test_' + location]['common_key'] == 'common_key_modified'
+        container = model_class(config).model_args
+        assert container[location + '/common_key'] == 'common_key_modified'
 
     @pytest.mark.parametrize('location', LOCATIONS - set(['block']))
     def test_loc(self, location, model_and_config):
         """ It is possible to directly send parameters to desired location.
         Those keys will not appear in other places.
         """
-        model, config = model_and_config
+        model_class, config = model_and_config
         destination = location + '/' + location + '_key'
         value = location + '_value_modified'
         config[destination] = value
-        container = model(config).test_container
-        assert container['test_' + location][location + '_key'] == value # check that key is delivered
+        container = model_class(config).model_args
+        assert container[destination] == value # check that key is delivered
         for loc in LOCATIONS - set([location, 'block']):
-            assert value not in container['test_' + loc].values() # check that key is not present in other places
+            assert value not in container[loc].values() # check that key is not present in other places
 
     @pytest.mark.parametrize('location', LOCATIONS - set(['block']))
     def test_loc_priority(self, location, model_and_config):
         """ Parameters, passed to a certain location, take priority over 'common' keys. """
-        model, config = model_and_config
-        config['common'] = {location + '_key': 'wrong_value'}
+        model_class, config = model_and_config
+        config['common/' + location + '_key'] = 'wrong_value'
         destination = location + '/' + location + '_key'
         value = location + '_value_modified'
         config[destination] = value
-        container = model(config).test_container
-        assert container['test_' + location][location + '_key'] == value
+        container = model_class(config).model_args
+        assert container[destination] == value
 
     def test_block(self, model_and_config):
         """ If model structure is nested (e.g. block inside body), inner
         parts inherit properties from outer (e.g. block has all of the keys
         passed to body).
         """
-        model, config = model_and_config
+        model_class, config = model_and_config
         config['body/body_key'] = 'body_key_modified'
         config['body/block/block_key'] = 'block_value_modified'
-        container = model(config).test_container
-        assert container['test_block']['body_key'] == 'body_key_modified'
-        assert container['test_block']['block_key'] == 'block_value_modified'
+        container = model_class(config).model_args
+        assert container['block/body_key'] == 'body_key_modified'
+        assert container['block/block_key'] == 'block_value_modified'
 
     def test_block_priority(self, model_and_config):
         """ If the same parameter is defined both in outer and inner part,
         the inner takes priority.
         """
-        model, config = model_and_config
+        model_class, config = model_and_config
         config['body/block_key'] = 'wrong_value'
         config['body/block/block_key'] = 'block_value_modified'
-        container = model(config).test_container
-        assert container['test_block']['block_key'] == 'block_value_modified'
+        container = model_class(config).model_args
+        assert container['block/block_key'] == 'block_value_modified'
--- batchflow/tests/dataformat_test.py
+++ batchflow/tests/dataformat_test.py
@@ -1,17 +1,11 @@
 """ Test for passing information through config. """
-# pylint: disable=import-error, wrong-import-position, no-name-in-module
-# pylint: disable=missing-docstring, redefined-outer-name
-import os
-import warnings
-warnings.filterwarnings("ignore")
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
+# pylint: disable=import-error, no-name-in-module
+# pylint: disable=redefined-outer-name
 import pytest
 import numpy as np
-import tensorflow as tf
 
 from batchflow import Pipeline, ImagesBatch, Dataset
-from batchflow import B, V
+from batchflow import B, V, C
 from batchflow.models.tf import VGG7, ResNet18, Inception_v1
 
 
@@ -25,8 +19,8 @@ def model_setup():
 
     Parameters
     ----------
-    d_f: {'channels_last', 'channels_first'}
-        desired format of returns.
+    data_format: {'channels_last', 'channels_first'}
+        Desired format of returns.
 
     Returns
     -------
@@ -34,10 +28,10 @@ def model_setup():
         First element is instance of Dataset.
         Second element is dict with model description.
     """
-    def _model_setup(d_f):
-        if d_f == 'channels_last':
+    def _model_setup(data_format):
+        if data_format == 'channels_last':
             shape_in = (100, 100, 2)
-        elif d_f == 'channels_first':
+        elif data_format == 'channels_first':
             shape_in = (2, 100, 100)
 
         size = 50
@@ -57,7 +51,7 @@ def model_setup():
     return _model_setup
 
 @pytest.fixture()
-def get_model_pipeline():
+def model_pipeline():
     """ Creates instance of Pipeline that is configured to use given model
     with passed parameters.
 
@@ -76,20 +70,19 @@ def get_model_pipeline():
         Test pipeline that consists of initialization of model and
         preparing for training with given config.
     """
-    def _get_model_pipeline(model_class, current_config):
-        test_pipeline = (Pipeline()
-                         .init_variable('current_loss')
-                         .init_model('dynamic', model_class,
-                                     'TestModel', current_config)
-                         .to_array()
-                         .train_model('TestModel',
-                                      fetches='loss',
-                                      images=B('images'),
-                                      labels=B('labels'),
-                                      save_to=V('current_loss'))
-                         )
-        return test_pipeline
-    return _get_model_pipeline
+
+    test_pipeline = (Pipeline()
+                     .init_variable('current_loss')
+                     .init_model('dynamic', C('model_class'),
+                                 'TestModel', C('model_config'))
+                     .to_array()
+                     .train_model('TestModel',
+                                  fetches='loss',
+                                  images=B('images'),
+                                  labels=B('labels'),
+                                  save_to=V('current_loss'))
+                     )
+    return test_pipeline
 
 
 
@@ -110,38 +103,38 @@ class Test_dataformat():
     @pytest.mark.parametrize('location', LOCATIONS)
     def test_default_dataformat(self, location, model_and_config):
         """ Default value for 'data_format' is 'channels_last'. """
-        model, config = model_and_config
-        container = model(config).test_container
-        assert container['test_' + location]['data_format'] == 'channels_last'
+        model_class, config = model_and_config
+        container = model_class(config).model_args
+        assert container[location]['data_format'] == 'channels_last'
 
     @pytest.mark.parametrize('location', LOCATIONS)
     def test_common_dataformat(self, location, model_and_config):
         """ Easiest way to change 'data_format' for every part of network is to
         pass it to 'common'.
         """
-        model, config = model_and_config
+        model_class, config = model_and_config
         config['common/data_format'] = 'channels_first'
-        container = model(config).test_container
-        assert container['test_' + location]['data_format'] == 'channels_first'
+        container = model_class(config).model_args
+        assert container[location + '/data_format'] == 'channels_first'
 
     @pytest.mark.parametrize('location', LOCATIONS - set(['block']))
     def test_loc_dataformat(self, location, model_and_config):
         """ 'data_format' can be passed directly to desired location. """
-        model, config = model_and_config
+        model_class, config = model_and_config
         destination = location + '/data_format'
         config[destination] = 'channels_first'
-        container = model(config).test_container
-        assert container['test_' + location]['data_format'] == 'channels_first'
+        container = model_class(config).model_args
+        assert container[destination] == 'channels_first'
         for loc in LOCATIONS - set([location, 'block']):
-            assert 'channels_first' not in container['test_' + loc].values()
+            assert 'channels_first' not in container[loc].values()
 
     def test_block_dataformat(self, model_and_config):
         """ Parameters, passed to inner parts take priority over outers. """
-        model, config = model_and_config
+        model_class, config = model_and_config
         config['body/data_format'] = 'channels_last'
         config['body/block/data_format'] = 'channels_first'
-        container = model(config).test_container
-        assert container['test_block']['data_format'] == 'channels_first'
+        container = model_class(config).model_args
+        assert container['block/data_format'] == 'channels_first'
 
 
 
@@ -156,32 +149,32 @@ class Test_models:
         to train it on a small batch.
 
     """
-    def test_last_default(self, model, model_setup, get_model_pipeline):
+    def test_last_default(self, model, model_setup, model_pipeline):
         """ Default value for 'data_format' is 'channels_last'. """
-        fake_dataset, config = model_setup(d_f='channels_last')
-
-        test_pipeline = get_model_pipeline(model, config)
+        fake_dataset, config = model_setup(data_format='channels_last')
+        pipeline_config = {'model_class': model, 'model_config': config}
+        test_pipeline = model_pipeline.set_config(pipeline_config)
         total_pipeline = test_pipeline << fake_dataset
-        n_b = total_pipeline.next_batch(7, n_epochs=None)
-        assert len(n_b.index) == 7
+        batch = total_pipeline.next_batch(7, n_epochs=None)
+        assert len(batch) == 7
 
-    def test_last_common(self, model, model_setup, get_model_pipeline):
+    def test_last_common(self, model, model_setup, model_pipeline):
         """ We can explicitly pass 'data_format', it has no effect in this case. """
-        fake_dataset, config = model_setup(d_f='channels_last')
+        fake_dataset, config = model_setup(data_format='channels_last')
         config['common/data_format'] = 'channels_last'
-        test_pipeline = get_model_pipeline(model, config)
+        pipeline_config = {'model_class': model, 'model_config': config}
+        test_pipeline = model_pipeline.set_config(pipeline_config)
         total_pipeline = test_pipeline << fake_dataset
-        n_b = total_pipeline.next_batch(7, n_epochs=None)
-        assert len(n_b.index) == 7
+        batch = total_pipeline.next_batch(7, n_epochs=None)
+        assert len(batch) == 7
 
     @pytest.mark.xfail(run=True)
-    def test_first_common(self, model, model_setup, get_model_pipeline):
+    def test_first_common(self, model, model_setup, model_pipeline):
         """ That is intended way to communicate 'data_format' with model. """
-        if int(tf.__version__.split('.')[1]) < 12:
-            pytest.skip('too old to work')
-        fake_dataset, config = model_setup(d_f='channels_first')
+        fake_dataset, config = model_setup(data_format='channels_first')
         config['common/data_format'] = 'channels_first'
-        test_pipeline = get_model_pipeline(model, config)
+        pipeline_config = {'model_class': model, 'model_config': config}
+        test_pipeline = model_pipeline.set_config(pipeline_config)
         total_pipeline = test_pipeline << fake_dataset
-        n_b = total_pipeline.next_batch(7, n_epochs=None)
-        assert len(n_b.index) == 7
+        batch = total_pipeline.next_batch(7, n_epochs=None)
+        assert len(batch) == 7
