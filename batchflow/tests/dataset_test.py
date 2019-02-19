a76f2656cb290e47d0faa1253dacef0f595b29f1
pphator <ppahtor@gmail.com>
2019-02-19 16:16:04 +0300

    Add new tests and fix errors

--- batchflow/tests/dataset_test.py
+++ batchflow/tests/dataset_test.py
@@ -1,9 +1,9 @@
-# pylint: disable=missing-docstring
+# pylint: disable=missing-docstring, redefined-outer-name
 
 import pytest
 import numpy as np
 
-from batchflow import Dataset, Batch, DatasetIndex, Pipeline
+from batchflow import Dataset, Batch, ImagesBatch, DatasetIndex, Pipeline
 
 
 @pytest.fixture
@@ -17,7 +17,14 @@ class TestDataset:
         new_index = DatasetIndex(np.arange(25))
         new_ds = Dataset.from_dataset(dataset, new_index)
         assert isinstance(new_ds, Dataset)
+        assert new_ds.batch_class == dataset.batch_class
         assert (new_ds.index.index == new_index.index).all()
+
+    def test_from_dataset_new_batch_class(self, dataset):
+        new_index = DatasetIndex(np.arange(25))
+        new_ds = Dataset.from_dataset(dataset, new_index, batch_class=ImagesBatch)
+        assert isinstance(new_ds, Dataset)
+        assert new_ds.batch_class == ImagesBatch
 
     def test_build_index(self):
         new_index = Dataset.build_index(np.arange(25))
@@ -27,16 +34,18 @@ class TestDataset:
         new_index = DatasetIndex(np.arange(25))
         new_ds = dataset.create_subset(new_index)
         assert isinstance(new_ds, dataset.__class__)
-        assert np.isin(new_ds.index.index, dataset.index.index).all()
+        assert np.isin(new_ds.indices, dataset.indices).all()
+
+    def test_create_subset_wrong_index(self, dataset):
+        wrong_index = DatasetIndex(np.arange(200, 225))
+        new_ds = dataset.create_subset(wrong_index)
+        assert np.isin(new_ds.indices, dataset.indices).all()
 
     def test_create_batch(self, dataset):
         target_index = DatasetIndex(np.arange(5))
-        uncorrect_new_index = DatasetIndex(np.arange(100, 110))
         new_batch = dataset.create_batch(target_index)
         assert isinstance(new_batch, dataset.batch_class)
-        assert len(new_batch.index.index) == len(target_index.index)
-        with pytest.raises(ValueError):
-            dataset.create_batch(uncorrect_new_index)
+        assert len(new_batch.indices) == len(target_index.indices)
 
     def test_pipeline(self, dataset):
         pipeline_config = {}
