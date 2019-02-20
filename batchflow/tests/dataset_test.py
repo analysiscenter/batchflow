fe8cbb8c09199bd873e2b14ee2b9dc8454faf50c
pphator <ppahtor@gmail.com>
2019-02-20 16:30:23 +0300

    Update dataset tests
    Fix create_subset test
    Correct mistakes in accordance to suggestions

--- batchflow/tests/dataset_test.py
+++ batchflow/tests/dataset_test.py
@@ -8,48 +8,42 @@ from batchflow import Dataset, Batch, ImagesBatch, DatasetIndex, Pipeline
 
 @pytest.fixture
 def dataset():
-    index = DatasetIndex(np.arange(100))
+    index = DatasetIndex(100)
     return Dataset(index, Batch)
 
 
 class TestDataset:
     def test_from_dataset(self, dataset):
-        new_index = DatasetIndex(np.arange(25))
-        new_ds = Dataset.from_dataset(dataset, new_index)
-        assert isinstance(new_ds, Dataset)
+        new_index = DatasetIndex(25)
+        new_ds = dataset.from_dataset(dataset, new_index)
+        assert isinstance(new_ds, dataset.__class__)
         assert new_ds.batch_class == dataset.batch_class
         assert (new_ds.index.index == new_index.index).all()
 
     def test_from_dataset_new_batch_class(self, dataset):
-        new_index = DatasetIndex(np.arange(25))
+        new_index = DatasetIndex(25)
         new_ds = Dataset.from_dataset(dataset, new_index, batch_class=ImagesBatch)
-        assert isinstance(new_ds, Dataset)
+        assert isinstance(new_ds, dataset.__class__)
         assert new_ds.batch_class == ImagesBatch
 
     def test_build_index(self):
-        new_index = Dataset.build_index(np.arange(25))
+        new_index = Dataset.build_index(25)
         assert isinstance(new_index, DatasetIndex)
 
     def test_create_subset(self, dataset):
-        new_index = DatasetIndex(np.arange(25))
+        new_index = DatasetIndex(25)
         new_ds = dataset.create_subset(new_index)
         assert isinstance(new_ds, dataset.__class__)
         assert np.isin(new_ds.indices, dataset.indices).all()
 
     def test_create_subset_wrong_index(self, dataset):
-        """
-        This method checks for correct execution create_subset method in case of
-        there should be exception during calling this method with index which is not in
-        source dataset index range.
-        Now we raises and handle AssertError
-        """
         wrong_index = DatasetIndex(np.arange(200, 225))
-        with pytest.raises(AssertionError):
-            new_ds = dataset.create_subset(wrong_index)
-            assert np.isin(new_ds.indices, dataset.indices).all()
+        with pytest.raises(IndexError) as error:
+            dataset.create_subset(wrong_index)
+            assert 'IndexError' in str(error)
 
     def test_create_batch(self, dataset):
-        target_index = DatasetIndex(np.arange(5))
+        target_index = DatasetIndex(5)
         new_batch = dataset.create_batch(target_index)
         assert isinstance(new_batch, dataset.batch_class)
         assert len(new_batch.indices) == len(target_index.indices)
@@ -67,6 +61,7 @@ class TestDataset:
 
     def test_split(self, dataset):
         assert dataset.train is None
-        dataset.split()
+        dataset.split((0.8, 0.2))
         assert dataset.train is not None
         assert dataset.test is not None
+        assert dataset.validation is None
