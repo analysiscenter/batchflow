9344854fd2e027756ca40743b16165396523df84
Dmitry Grigoriev <grigd2013@gmail.com>
2019-02-28 17:42:23 +0500

    Accuracy_test

---
+++ batchflow/tests/accuracy_test.py
@@ -0,0 +1,40 @@
+import pytest
+from batchflow.models import metrics
+import numpy as np
+	
+def test_different_shapes():
+		
+	y_true, y_pred = np.random.choice( [0, 1], size = (5,) ), np.random.choice( [0, 1], size = (4,4) ) 
+		
+	with pytest.raises(ValueError):
+			
+		metrics.ClassificationMetrics(y_true, y_pred, fmt = 'labels', num_classes = 2 ).accuracy()
+		
+def test_single_value_two_class_classification():
+
+	y_true, y_pred = np.random.choice( [0, 1], size = (5,) ), np.random.choice( [0, 1], size = (5,) )
+
+	assert isinstance( metrics.ClassificationMetrics(y_true, y_pred, fmt = 'labels', num_classes = 2).accuracy(),
+           np.floating )
+		   
+def test_vector_batches_two_class_classification():
+
+	y_true, y_pred = np.array( [[[1,1],[0,1]],[[0,1],[1,1]],[[1,0],[1,1]]] ), np.array( [[[0,1],[1,1]],[[1,0],[0,0]],[[0,0],[0,1]]])
+	
+	assert metrics.ClassificationMetrics(y_true, y_pred, fmt = 'labels', num_classes = 2).accuracy().shape[0] > 1
+           	
+	
+def test_accuracy_between_zero_and_one():
+	
+	y_true, y_pred = np.random.choice( [0, 1], size = (5,) ), np.random.choice( [0, 1], size = (5,) )
+	
+	assert  ( 0 <= metrics.ClassificationMetrics(y_true, y_pred, fmt = 'labels', num_classes = 2).accuracy()  )  &  ( metrics.ClassificationMetrics(y_true, y_pred, fmt='labels', num_classes=2).accuracy() <= 1 )
+	
+def test_confusion_matrix():
+
+	y_true, y_pred = np.array ( [1, 1, 0, 1, 0, 0] ), np.array( [0, 0, 1, 0, 0, 0] )
+	
+	conf_matrix = np.array( [ [2, 3], [1, 0] ] )
+	
+	assert ( metrics.ClassificationMetrics(y_true, y_pred,fmt='labels',num_classes=2)._confusion_matrix == conf_matrix ).all()
+	
