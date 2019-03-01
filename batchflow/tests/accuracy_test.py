04590ea44b91e20f94b8aa320be93b433be52b59
Dmitry Grigoriev <grigd2013@gmail.com>
2019-03-01 15:30:29 +0500

    Add two tests: accuracy calculation and f1 vs accuracy

--- batchflow/tests/accuracy_test.py
+++ batchflow/tests/accuracy_test.py
@@ -38,3 +38,23 @@ def test_confusion_matrix():
 	
 	assert ( metrics.ClassificationMetrics(y_true, y_pred,fmt='labels',num_classes=2)._confusion_matrix == conf_matrix ).all()
 	
+def test_accuracy_calculation():
+
+	y_true, y_pred = np.array ( [1, 1, 0, 1, 0, 0] ), np.array( [0, 0, 1, 0, 0, 0] )
+	
+	TP, FP, FN, TN = 0, 1, 3, 2
+	
+	accuracy = (TP + TN) / ( TP + TN + FP + FN ) 
+	
+	assert accuracy == metrics.ClassificationMetrics(y_true, y_pred,fmt='labels',num_classes=2).accuracy()
+	
+def test_f1_vs_accuracy_imbalanced_classes():
+
+		y_true, y_pred = np.array( [0,0,0,0,1] ), np.array( [0, 0, 0, 0, 0] )
+    	
+		f1_score_result = metrics.ClassificationMetrics(y_true,y_pred, fmt = 'labels', num_classes = 2).f1_score()
+		
+		accuracy_result = metrics.ClassificationMetrics(y_true,y_pred, fmt = 'labels', num_classes = 2).accuracy()
+		
+		assert f1_score_result < accuracy_result
+	
