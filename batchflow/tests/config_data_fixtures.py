9e2e9a81e17d082bac8d453038ed5c3924cc4304
cdtn <stevesinobi@gmail.com>
2019-03-16 22:41:40 +0300

    Pylint spaces stuff fixed

--- batchflow/tests/config_data_fixtures.py
+++ batchflow/tests/config_data_fixtures.py
@@ -17,35 +17,35 @@ from batchflow import Config
 def data_dict_init(request):
     """
     Generates data set for test_dict_init consisting of:
-        1. «process» — nested dictionary with slashed indexing 
+        1. «process» — nested dictionary with slashed indexing
         2. «expect» — normal nested dictionary
 
     After being both passed to Config class constructor contents of obtained class instances are expected to match.
     """
     data = [
         dict(
-            process = {'a/b' : 1},
-            expect = {'a': {'b' : 1}}
+            process={'a/b' : 1},
+            expect={'a': {'b' : 1}}
             ),
 
         dict(
-            process = {'a/b' : 1, 'a/c' : 2},
-            expect = {'a' : {'b' : 1, 'c' : 2}}
-            ),
-        
-        dict(
-            process = {'a' : 1, 'a/b' : 2, 'a/c' : 3},
-            expect = {'a' : {'b' : 2, 'c' : 3}}
+            process={'a/b' : 1, 'a/c' : 2},
+            expect={'a' : {'b' : 1, 'c' : 2}}
             ),
 
         dict(
-            process = {'a' : {'b' : 1}, 'a/c' : 2},
-            expect = {'a' : {'b' : 1, 'c' : 2}}
+            process={'a' : 1, 'a/b' : 2, 'a/c' : 3},
+            expect={'a' : {'b' : 2, 'c' : 3}}
             ),
 
         dict(
-            process = {'a' : Config({'b' : 2})},
-            expect = {'a' : {'b' : 2}}
+            process={'a' : {'b' : 1}, 'a/c' : 2},
+            expect={'a' : {'b' : 1, 'c' : 2}}
+            ),
+
+        dict(
+            process={'a' : Config({'b' : 2})},
+            expect={'a' : {'b' : 2}}
             )
         ]
 
@@ -62,28 +62,28 @@ def data_list_init(request):
     """
     data = [
         dict(
-            process = [('a/b', 1)],
-            expect = {'a' : {'b' : 1}}
+            process=[('a/b', 1)],
+            expect={'a' : {'b' : 1}}
             ),
 
         dict(
-            process = [('a/b', 1), ('a/c', 2)],
-            expect = {'a' : {'b' : 1, 'c' : 2}}
+            process=[('a/b', 1), ('a/c', 2)],
+            expect={'a' : {'b' : 1, 'c' : 2}}
             ),
 
         dict(
-            process = [('a', 1), ('a/b', 2), ('a/c', 3)],
-            expect = {'a' : {'b' : 2, 'c' : 3}}
+            process=[('a', 1), ('a/b', 2), ('a/c', 3)],
+            expect={'a' : {'b' : 2, 'c' : 3}}
             ),
 
         dict(
-            process = [('a', {'b' : 1}), ('a/c', 2)],
-            expect = {'a' : {'b' : 1, 'c' : 2}}
+            process=[('a', {'b' : 1}), ('a/c', 2)],
+            expect={'a' : {'b' : 1, 'c' : 2}}
             ),
 
         dict(
-            process = [('a', Config([('b',2)]))],
-            expect = {'a' : {'b' : 2}}
+            process=[('a', Config([('b', 2)]))],
+            expect={'a' : {'b' : 2}}
             )
         ]
 
@@ -101,8 +101,8 @@ def data_config_init(request):
     """
     data = [
         dict(
-            process = Config({'a' : 0}),
-            expect = {'a' : 0}
+            process=Config({'a' : 0}),
+            expect={'a' : 0}
             )
         ]
 
@@ -119,17 +119,17 @@ def data_pop(request):
     """
     data = [
         dict(
-            process = Config({'a' : {'b' : 1, 'c' : 2}}),
-            key = 'a/b',
-            expect_value = 1,
-            expect_config = Config({'a' : {'c' : 2}})
+            process=Config({'a' : {'b' : 1, 'c' : 2}}),
+            key='a/b',
+            expect_value=1,
+            expect_config=Config({'a' : {'c' : 2}})
             ),
 
         dict(
-            process = Config({'a' : {'b' : {'c' : 1, 'd' : 2}}}),
-            key = 'a/b',
-            expect_value = {'c' : 1, 'd' : 2},
-            expect_config = Config({'a' : {}})
+            process=Config({'a' : {'b' : {'c' : 1, 'd' : 2}}}),
+            key='a/b',
+            expect_value={'c' : 1, 'd' : 2},
+            expect_config=Config({'a' : {}})
             )
         ]
 
@@ -146,17 +146,17 @@ def data_get(request):
     """
     data = [
         dict(
-            process = Config({'a' : {'b' : 1, 'c' : 2}}),
-            key = 'a/b',
-            expect_value = 1,
-            expect_config = Config({'a' : {'b' : 1, 'c' : 2}})
+            process=Config({'a' : {'b' : 1, 'c' : 2}}),
+            key='a/b',
+            expect_value=1,
+            expect_config=Config({'a' : {'b' : 1, 'c' : 2}})
             ),
 
         dict(
-            process = Config({'a' : {'b' : {'c' : 1}}}),
-            key = 'a/b',
-            expect_value = {'c' : 1},
-            expect_config = Config({'a' : {'b' : {'c' : 1}}})
+            process=Config({'a' : {'b' : {'c' : 1}}}),
+            key='a/b',
+            expect_value={'c' : 1},
+            expect_config=Config({'a' : {'b' : {'c' : 1}}})
             )
         ]
 
@@ -173,24 +173,24 @@ def data_put(request):
     """
     data = [
         dict(
-            process = Config({'a' : 1}),
-            key = 'b',
-            value = 2,
-            expect = Config({'a' : 1, 'b' : 2})
+            process=Config({'a' : 1}),
+            key='b',
+            value=2,
+            expect=Config({'a' : 1, 'b' : 2})
             ),
 
         dict(
-            process = Config({'a' : {'b' : 1}}),
-            key = 'a/c',
-            value = 2,
-            expect = Config({'a' : {'b' : 1, 'c' : 2}})
+            process=Config({'a' : {'b' : 1}}),
+            key='a/c',
+            value=2,
+            expect=Config({'a' : {'b' : 1, 'c' : 2}})
             ),
 
         dict(
-            process = Config({'a' : {}}),
-            key = 'a/b',
-            value = {'c/d' : 1},
-            expect = Config({'a/b/c/d' : 1})
+            process=Config({'a' : {}}),
+            key='a/b',
+            value={'c/d' : 1},
+            expect=Config({'a/b/c/d' : 1})
             )
         ]
 
@@ -206,21 +206,21 @@ def data_flatten(request):
     """
     data = [
         dict(
-            process = Config({'a' : {'b' : 1}, 'c' : 2}),
-            expect_return = {'a/b' : 1, 'c' : 2},
-            expect_config = Config({'a' : {'b' : 1}, 'c' : 2})
+            process=Config({'a' : {'b' : 1}, 'c' : 2}),
+            expect_return={'a/b' : 1, 'c' : 2},
+            expect_config=Config({'a' : {'b' : 1}, 'c' : 2})
             ),
 
         dict(
-            process = Config({'a' : {'b' : 1, 'c' : {'d' : 2}}}),
-            expect_return = {'a/b' : 1, 'a/c/d' : 2},
-            expect_config = Config({'a' : {'b' : 1, 'c' : {'d' : 2}}})
+            process=Config({'a' : {'b' : 1, 'c' : {'d' : 2}}}),
+            expect_return={'a/b' : 1, 'a/c/d' : 2},
+            expect_config=Config({'a' : {'b' : 1, 'c' : {'d' : 2}}})
             ),
 
         dict(
-            process = Config({'a' : {'b' : {}, 'c' : None}}),
-            expect_return = {'a/b' : dict(), 'a/c' : None},
-            expect_config = Config({'a' : {'b' : {}, 'c' : None}})
+            process=Config({'a' : {'b' : {}, 'c' : None}}),
+            expect_return={'a/b' : dict(), 'a/c' : None},
+            expect_config=Config({'a' : {'b' : {}, 'c' : None}})
             )
         ]
 
@@ -238,27 +238,27 @@ def data_add(request):
     """
     data = [
         dict(
-            process_left = Config({'a' : 1, 'b' : 2}),
-            process_right = Config({'b' : 3, 'c' : 4}),
-            expect_return = Config({'a' : 1, 'b' : 3, 'c' : 4}),
-            expect_left = Config({'a' : 1, 'b' : 2}),
-            expect_right = Config({'b' : 3, 'c' : 4})
+            process_left=Config({'a' : 1, 'b' : 2}),
+            process_right=Config({'b' : 3, 'c' : 4}),
+            expect_return=Config({'a' : 1, 'b' : 3, 'c' : 4}),
+            expect_left=Config({'a' : 1, 'b' : 2}),
+            expect_right=Config({'b' : 3, 'c' : 4})
             ),
 
         dict(
-            process_left = Config({'a' : {'b' : {'c' : 1, 'd' : 2}}, 'b' : 3}),
-            process_right = Config({'a/b/c/d/e' : 4, 'a/b/c/d/f' : 5}),
-            expect_return = Config({'a' : {'b' : {'c' : {'d' : {'e' : 4, 'f' : 5}}, 'd' : 2}}, 'b' : 3}),
-            expect_left = Config({'a' : {'b' : {'c' : 1, 'd' : 2}}, 'b' : 3}),
-            expect_right = Config({'a/b/c/d/e' : 4, 'a/b/c/d/f' : 5})
+            process_left=Config({'a' : {'b' : {'c' : 1, 'd' : 2}}, 'b' : 3}),
+            process_right=Config({'a/b/c/d/e' : 4, 'a/b/c/d/f' : 5}),
+            expect_return=Config({'a' : {'b' : {'c' : {'d' : {'e' : 4, 'f' : 5}}, 'd' : 2}}, 'b' : 3}),
+            expect_left=Config({'a' : {'b' : {'c' : 1, 'd' : 2}}, 'b' : 3}),
+            expect_right=Config({'a/b/c/d/e' : 4, 'a/b/c/d/f' : 5})
             ),
 
         dict(
-            process_left = Config({'a' : {'b' : 1, 'c' : 2}, 'b' : {'d' : dict()}}),
-            process_right = Config({'a' : dict(), 'b' : None}),
-            expect_return = Config({'a' : {'b' : 1, 'c' : 2}, 'b' : None}),
-            expect_left = Config({'a' : {'b' : 1, 'c' : 2}, 'b' : {'d' : dict()}}),
-            expect_right = Config({'a' : dict(), 'b' : None})
+            process_left=Config({'a' : {'b' : 1, 'c' : 2}, 'b' : {'d' : dict()}}),
+            process_right=Config({'a' : dict(), 'b' : None}),
+            expect_return=Config({'a' : {'b' : 1, 'c' : 2}, 'b' : None}),
+            expect_left=Config({'a' : {'b' : 1, 'c' : 2}, 'b' : {'d' : dict()}}),
+            expect_right=Config({'a' : dict(), 'b' : None})
             )
         ]
 
@@ -275,17 +275,17 @@ def data_items(request):
     """
     data = [
         dict(
-            process = Config({'a' : {'b' : 1, 'c' : 2}}),
-            expect_full = [('a', {'b': 1, 'c': 2})],
-            expect_flat = [('a/b', 1), ('a/c', 2)],
-            expect_config = Config({'a' : {'b' : 1, 'c' : 2}})
+            process=Config({'a' : {'b' : 1, 'c' : 2}}),
+            expect_full=[('a', {'b': 1, 'c': 2})],
+            expect_flat=[('a/b', 1), ('a/c', 2)],
+            expect_config=Config({'a' : {'b' : 1, 'c' : 2}})
             ),
 
         dict(
-            process = Config({'a' : {'b' : None}}),
-            expect_full = [('a', {'b' : None})],
-            expect_flat = [('a/b', None)],
-            expect_config = Config({'a' : {'b' : None}})
+            process=Config({'a' : {'b' : None}}),
+            expect_full=[('a', {'b' : None})],
+            expect_flat=[('a/b', None)],
+            expect_config=Config({'a' : {'b' : None}})
             )
         ]
 
@@ -302,17 +302,17 @@ def data_keys(request):
     """
     data = [
         dict(
-            process = Config({'a' : {'b' : 1, 'c' : 2}}),
-            expect_full = ['a'],
-            expect_flat = ['a/b', 'a/c'],
-            expect_config = Config({'a' : {'b' : 1, 'c' : 2}})
+            process=Config({'a' : {'b' : 1, 'c' : 2}}),
+            expect_full=['a'],
+            expect_flat=['a/b', 'a/c'],
+            expect_config=Config({'a' : {'b' : 1, 'c' : 2}})
             ),
 
         dict(
-            process = Config({'a' : {'b' : None}, 'c' : 1}),
-            expect_full = ['a', 'c'],
-            expect_flat = ['a/b', 'c'],
-            expect_config = Config({'a' : {'b' : None}, 'c' : 1})
+            process=Config({'a' : {'b' : None}, 'c' : 1}),
+            expect_full=['a', 'c'],
+            expect_flat=['a/b', 'c'],
+            expect_config=Config({'a' : {'b' : None}, 'c' : 1})
             )
         ]
 
@@ -329,17 +329,17 @@ def data_values(request):
     """
     data = [
         dict(
-            process = Config({'a' : {'b' : 1, 'c' : 2}}),
-            expect_full = [{'b': 1, 'c': 2}],
-            expect_flat = [1, 2],
-            expect_config = Config({'a' : {'b' : 1, 'c' : 2}})
+            process=Config({'a' : {'b' : 1, 'c' : 2}}),
+            expect_full=[{'b': 1, 'c': 2}],
+            expect_flat=[1, 2],
+            expect_config=Config({'a' : {'b' : 1, 'c' : 2}})
             ),
 
         dict(
-            process = Config({'a' : {'b' : None}, 'c' : 1}),
-            expect_full = [{'b' : None}, 1],
-            expect_flat = [None, 1],
-            expect_config = Config({'a' : {'b' : None}, 'c' : 1})
+            process=Config({'a' : {'b' : None}, 'c' : 1}),
+            expect_full=[{'b' : None}, 1],
+            expect_flat=[None, 1],
+            expect_config=Config({'a' : {'b' : None}, 'c' : 1})
             )
         ]
 
@@ -356,31 +356,31 @@ def data_update(request):
     """
     data = [
         dict(
-            process_left = Config({'a' : {'b' : 1, 'c' : 2}}),
-            process_right = Config({'a/b' : 3, 'a/d' : 4}),
-            expect_left = Config({'a' : {'b' : 3, 'c' : 2, 'd' : 4}}),
-            expect_right = Config({'a/b' : 3, 'a/d' : 4})
+            process_left=Config({'a' : {'b' : 1, 'c' : 2}}),
+            process_right=Config({'a/b' : 3, 'a/d' : 4}),
+            expect_left=Config({'a' : {'b' : 3, 'c' : 2, 'd' : 4}}),
+            expect_right=Config({'a/b' : 3, 'a/d' : 4})
             ),
 
         dict(
-            process_left = Config({'a' : {'b' : 1}}),
-            process_right = Config({'a' : {'c' : {'d' : 2}}}),
-            expect_left = Config({'a' : {'b' : 1, 'c' : {'d' : 2}}}),
-            expect_right = Config({'a' : {'c' : {'d' : 2}}})
+            process_left=Config({'a' : {'b' : 1}}),
+            process_right=Config({'a' : {'c' : {'d' : 2}}}),
+            expect_left=Config({'a' : {'b' : 1, 'c' : {'d' : 2}}}),
+            expect_right=Config({'a' : {'c' : {'d' : 2}}})
             ),
 
         dict(
-            process_left = Config({'a' : {}, 'b' : None}),
-            process_right = Config({'a' : None, 'b' : {}}),
-            expect_left = Config({'a' : None, 'b' : {}}),
-            expect_right = Config({'a' : None, 'b' : {}})
+            process_left=Config({'a' : {}, 'b' : None}),
+            process_right=Config({'a' : None, 'b' : {}}),
+            expect_left=Config({'a' : None, 'b' : {}}),
+            expect_right=Config({'a' : None, 'b' : {}})
             ),
 
         dict(
-            process_left = Config({'a' : {'b' : 1, 'c' : 2}, 'b' : {'d' : dict()}}),
-            process_right = Config({'a' : dict(), 'b' : 0}),
-            expect_left = Config({'a' : {'b' : 1, 'c' : 2}, 'b' : 0}),
-            expect_right = Config({'a' : dict(), 'b' : 0})
+            process_left=Config({'a' : {'b' : 1, 'c' : 2}, 'b' : {'d' : dict()}}),
+            process_right=Config({'a' : dict(), 'b' : 0}),
+            expect_left=Config({'a' : {'b' : 1, 'c' : 2}, 'b' : 0}),
+            expect_right=Config({'a' : dict(), 'b' : 0})
             )
         ]
 
