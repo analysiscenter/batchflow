be7f40600df9062265330731696635a4eb779458
cdtn <stevesinobi@gmail.com>
2019-03-16 20:17:51 +0300

    Done with tabs

--- batchflow/tests/conftest.py
+++ batchflow/tests/conftest.py
@@ -3,6 +3,6 @@
 import pytest
 
 from .config_pass_test import single_config, multi_config, model_and_config
-from .config_data_fixtures import (data_dict_init, data_list_init, data_config_init,
-									data_pop, data_get, data_put, data_flatten, data_add,
-									data_items, data_keys, data_values, data_update)
+from .config_data_fixtures import data_dict_init, data_list_init, data_config_init
+from .config_data_fixtures import data_pop, data_get, data_put, data_flatten, data_add
+from .config_data_fixtures import data_items, data_keys, data_values, data_update
