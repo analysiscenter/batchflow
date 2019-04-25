700c9a5056b6c60d2f2a477008df13503d2b0626
Roman Kh <rhudor@gmail.com>
2019-04-25 14:24:05 +0300

    Make ns pipelines picklable

--- batchflow/ns_pipeline.py
+++ batchflow/ns_pipeline.py
@@ -1,4 +1,5 @@
 """ Namespace pipeline """
+import sys
 from functools import partial
 import numpy as np
 
@@ -12,30 +13,50 @@ class NamespacePipeline:
         self._namespaces = list(namespaces)
         self._actions = []
 
+    @classmethod
+    def concat(cls, pipe1, pipe2):
+        """ Concatenate two pipelines """
+        # pylint: disable=protected-access
+        new_p = NamespacePipeline(pipe1.pipeline)
+        new_p._actions = pipe1._actions + pipe2._actions
+        new_p._namespaces = pipe1._namespaces + [a for a in pipe2._namespaces if a not in pipe1._namespaces]
+        return new_p
+
+    def __getstate__(self):
+        state = dict(actions=self._actions, namespaces=self._namespaces, pipeline=self.pipeline)
+        return state
+
+    def __setstate__(self, state):
+        self._actions = state['actions']
+        self._namespaces = state['namespaces']
+        self.pipeline = state['pipeline']
+
     def __add__(self, other):
         if isinstance(other, NamespacePipeline):
             return self.pipeline + other
         return other + self
 
+    @property
+    def _all_namespaces(self):
+        return [sys.modules["__main__"]] + self._namespaces
+
     def has_method(self, name):
-        return any(hasattr(namespace, name) for namespace in self._namespaces)
+        return any(hasattr(namespace, name) for namespace in self._all_namespaces)
 
     def get_method(self, name):
         """ Return a method by the name """
-        for namespace in self._namespaces:
+        for namespace in self._all_namespaces:
             if hasattr(namespace, name):
                 return getattr(namespace, name)
 
-    def _add_action(self, method, *args, save_to=None, **kwargs):
-        self._actions.append({'method': method, 'args': args, 'kwargs': kwargs,
-                              'save_to': save_to})
+    def _add_action(self, name, *args, save_to=None, **kwargs):
+        self._actions.append({'name': name, 'args': args, 'kwargs': kwargs, 'save_to': save_to})
         return self
 
     def __getattr__(self, name):
-        method = self.get_method(name)
-        if method is None:
-            raise AttributeError("Unknown method: %s" % name)
-        return partial(self._add_action, method)
+        if self.has_method(name):
+            return partial(self._add_action, name)
+        raise AttributeError("Unknown name: %s" % name)
 
     def add_namespace(self, *namespaces):
         self._namespaces.extend(namespaces)
@@ -44,7 +65,12 @@ class NamespacePipeline:
     def _exec_action(self, action):
         args_value = eval_expr(action['args'], pipeline=self.pipeline)
         kwargs_value = eval_expr(action['kwargs'], pipeline=self.pipeline)
-        res = action['method'](*args_value, **kwargs_value)
+
+        method = self.get_method(action['name'])
+        if method is None:
+            raise ValueError("Unknown method: %s" % action['name'])
+
+        res = method(*args_value, **kwargs_value)
 
         if isinstance(action['save_to'], NamedExpression):
             action['save_to'].set(res, pipeline=self.pipeline)
