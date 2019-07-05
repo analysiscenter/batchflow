b00c59a9f15fdeabbd38b6b9b13be16b22f89bb2
Sergey Tsimfer <Tsimfer.SA@gazprom-neft.ru>
2019-07-05 09:31:18 +0300

    Update DeepLab

--- batchflow/models/tf/deeplab.py
+++ batchflow/models/tf/deeplab.py
@@ -13,7 +13,7 @@ from . import EncoderDecoder, Xception
 
 class DeepLab(EncoderDecoder):
     """ DeepLab v3+ model archtecture.
-    By default, inpu is encoded with Xception backbone (entry, middle and exit flows),
+    By default, input is encoded with Xception backbone (entry, backbone_middle and backbone_exit flows),
     then worked via Atrous Spatial Pyramid Pooling layer, and, finally, restored to original
     spatial size with simple yet effective decoder module.
 
@@ -27,12 +27,12 @@ class DeepLab(EncoderDecoder):
         Dictionary with parameters for entry encoding: downsampling of the inputs.
         See :meth:`~.EncoderDecoder.encoder` arguments.
 
-        middle : dict
-        Dictionary with parameters for middle encoding: thorough processing.
+        backbone_middle : dict
+        Dictionary with parameters for backbone_middle encoding: thorough processing.
         See :meth:`~.EncoderDecoder.embedding` arguments.
 
-        exit : dict
-        Dictionary with parameters for exit encoding: final increase in number of features.
+        backbone_exit : dict
+        Dictionary with parameters for backbone_exit encoding: final increase in number of features.
         See :meth:`~.EncoderDecoder.embedding` arguments.
 
         embedding : dict
@@ -41,17 +41,15 @@ class DeepLab(EncoderDecoder):
     @classmethod
     def default_config(cls):
         config = super().default_config()
-        config['body/entry'] = dict(base=None, num_stages=None)
-        config['body/entry/downsample'] = dict(layout=None)
-        config['body/entry/blocks'] = dict(base=Xception.block, filters=None, strides=2, combine_type='conv')
+        config['body/encoder'] = dict(base=None, num_stages=None)
+        config['body/encoder/downsample'] = dict(layout=None)
+        config['body/encoder/blocks'] = dict(base=Xception.block, filters=None, strides=2, combine_type='conv')
 
-        config['body/middle'] = dict(base=Xception.block, num_stages=None,
-                                     filters=None, strides=1, combine_type='sum')
-
-        config['body/exit'] = dict(base=Xception.block, num_stages=None,
-                                   filters=None, strides=1, depth_activation=True, combine_type='conv')
-
+        config['body/embedding_common'] = dict(base=Xception.block)
+        config['body/backbone_middle'] = dict(num_stages=None, filters=None, strides=1, combine_type='sum')
+        config['body/backbone_exit'] = dict(num_stages=None, filters=None, strides=1, depth_activation=True, combine_type='conv')
         config['body/embedding'] = dict(base=aspp)
+        config['body/embedding_order'] = ['backbone_middle', 'backbone_exit', 'embedding']
 
         config['body/decoder'] = dict(skip=True, num_stages=None, factor=None)
         config['body/decoder/upsample'] = dict(layout='b')
@@ -59,34 +57,6 @@ class DeepLab(EncoderDecoder):
         config['head'] = dict(layout='c', kernel_size=1)
         return config
 
-
-    @classmethod
-    def body(cls, inputs, name='body', **kwargs):
-        """ Create encoder, embedding and decoder. """
-        kwargs = cls.fill_params('body', **kwargs)
-        entry = kwargs.pop('entry')
-        middle = kwargs.pop('middle')
-        exit = kwargs.pop('exit')
-        embedding = kwargs.pop('embedding')
-        decoder = kwargs.pop('decoder')
-
-        with tf.variable_scope(name):
-            # Entry flow: transition down
-            encoder_outputs = cls.encoder(inputs, name='entry', **entry, **kwargs)
-
-            # Middle flow: thorough processing without changing neither spatial resolution, nor number of filters
-            middle = cls.embedding(encoder_outputs[-1], name='middle', **middle, **kwargs)
-
-            # Exit flow: increase amount of features maps
-            exit = cls.embedding(middle, name='exit', **exit, **kwargs)
-
-            # Bottleneck: ASPP
-            x = cls.embedding(exit, **embedding, **kwargs)
-            encoder_outputs.append(x)
-
-            # Decoder: transition up
-            x = cls.decoder(encoder_outputs, **decoder, **kwargs)
-        return x
 
 
 class DeepLab8(DeepLab):
@@ -96,21 +66,21 @@ class DeepLab8(DeepLab):
         config = super().default_config()
         config['initial_block'] = dict(layout='cnacna', filters=32, kernel_size=3, strides=1)
 
-        config['body/entry/num_stages'] = 4
-        config['body/entry/blocks/filters'] = [[64]*3,
+        config['body/encoder/num_stages'] = 4
+        config['body/encoder/blocks/filters'] = [[64]*3,
                                                [128]*3,
                                                [256]*3,
                                                [728]*3]
-        config['body/entry/blocks/strides'] = [2, 2, 2, 1]
+        config['body/encoder/blocks/strides'] = [2, 2, 2, 1]
 
-        config['body/middle/num_stages'] = 8
-        config['body/middle/rate'] = 2 # not sure if actually needed
-        config['body/middle/filters'] = [[728]*3]*8
+        config['body/backbone_middle/num_stages'] = 8
+        config['body/backbone_middle/rate'] = 2 # not sure if actually needed
+        config['body/backbone_middle/filters'] = [[728]*3]*8
 
-        config['body/exit/num_stages'] = 2
-        config['body/exit/rate'] = [2, 4]
-        config['body/exit/filters'] = [[728, 1024, 1024],
-                                       [1536, 1536, 2048]]
+        config['body/backbone_exit/num_stages'] = 2
+        config['body/backbone_exit/rate'] = [2, 4]
+        config['body/backbone_exit/filters'] = [[728, 1024, 1024],
+                                                [1536, 1536, 2048]]
 
         config['body/embedding/rates'] = (12, 24, 36)
 
@@ -125,21 +95,21 @@ class DeepLab16(DeepLab):
         config = super().default_config()
         config['initial_block'] = dict(layout='cnacna', filters=32, kernel_size=3, strides=1)
 
-        config['body/entry/num_stages'] = 4
-        config['body/entry/blocks/filters'] = [[64]*3,
+        config['body/encoder/num_stages'] = 4
+        config['body/encoder/blocks/filters'] = [[64]*3,
                                                [128]*3,
                                                [256]*3,
                                                [728]*3]
-        config['body/entry/blocks/strides'] = [2, 2, 2, 2]
+        config['body/encoder/blocks/strides'] = [2, 2, 2, 2]
 
-        config['body/middle/num_stages'] = 16
-        config['body/middle/rate'] = 2 # not sure if actually needed
-        config['body/middle/filters'] = [[728]*3]*16
+        config['body/backbone_middle/num_stages'] = 16
+        config['body/backbone_middle/rate'] = 2 # not sure if actually needed
+        config['body/backbone_middle/filters'] = [[728]*3]*16
 
-        config['body/exit/num_stages'] = 2
-        config['body/exit/rate'] = [1, 2]
-        config['body/exit/filters'] = [[728, 1024, 1024],
-                                       [1536, 1536, 2048]]
+        config['body/backbone_exit/num_stages'] = 2
+        config['body/backbone_exit/rate'] = [1, 2]
+        config['body/backbone_exit/filters'] = [[728, 1024, 1024],
+                                                [1536, 1536, 2048]]
 
         config['body/embedding/rates'] = (6, 12, 18)
 
@@ -154,21 +124,21 @@ class DeepLabS(DeepLab):
         config = super().default_config()
         config['initial_block'] = dict(layout='cna', filters=32, kernel_size=3, strides=1)
 
-        config['body/entry/num_stages'] = 4
-        config['body/entry/blocks/filters'] = [[6]*3,
+        config['body/encoder/num_stages'] = 4
+        config['body/encoder/blocks/filters'] = [[6]*3,
                                                [1]*3,
                                                [2]*3,
                                                [7]*3]
-        config['body/entry/blocks/strides'] = [2, 2, 2, 2]
+        config['body/encoder/blocks/strides'] = [2, 2, 2, 2]
 
-        config['body/middle/num_stages'] = 4
-        config['body/middle/rate'] = 2 # not sure if actually needed
-        config['body/middle/filters'] = [[7]*3]*4
+        config['body/backbone_middle/num_stages'] = 4
+        config['body/backbone_middle/rate'] = 2 # not sure if actually needed
+        config['body/backbone_middle/filters'] = [[7]*3]*4
 
-        config['body/exit/num_stages'] = 2
-        config['body/exit/rate'] = [2, 4]
-        config['body/exit/filters'] = [[7, 10, 10],
-                                       [10, 10, 12]]
+        config['body/backbone_exit/num_stages'] = 2
+        config['body/backbone_exit/rate'] = [2, 4]
+        config['body/backbone_exit/filters'] = [[7, 10, 10],
+                                                [10, 10, 12]]
 
         config['body/embedding/rates'] = (6, 12, 18)
 
