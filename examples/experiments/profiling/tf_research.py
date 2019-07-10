b41ef1ea78012585ff36802c473574ddf2b45e40
alexeykozhevin <a.kozhevin@analysiscenter.org>
2019-07-10 14:40:46 +0300

    Remove root and branch from scripts

--- examples/experiments/profiling/tf_research.py
+++ examples/experiments/profiling/tf_research.py
@@ -56,8 +56,8 @@ def get_accuracy(iteration, experiment, pipeline):
     return metrics.evaluate('accuracy')
 
 research = (Research()
-    .add_pipeline(root=train_root, branch=train_template, variables='loss', name='train')
-    .add_pipeline(root=test_root, branch=test_template, name='test', run=True, execute='%100', import_from='train')
+    .add_pipeline(train_ppl, variables='loss', name='train')
+    .add_pipeline(test_ppl, branch=test_template, name='test', run=True, execute='%100', import_from='train')
     .add_function(get_accuracy, returns='accuracy', name='test_accuracy', execute='%100', pipeline='test')
 )
 
