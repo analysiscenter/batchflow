05cddc81531e62b41f328a69d292b723aa8df6df
Sergey Tsimfer <Tsimfer.SA@gazprom-neft.ru>
2019-08-05 12:42:39 +0300

    Add device configurations

--- batchflow/tests/notebooks_test.py
+++ batchflow/tests/notebooks_test.py
@@ -1,11 +1,24 @@
 """ Run multiple notebooks. """
+import os
 import warnings
 from glob import glob
 import pytest
 
+from tensorflow.test import is_gpu_available
 
 
-BAD_PREFIXES = ['get_ipython', 'plt', 'plot', 'figure', 'ax.',]
+
+NO_GPU = pytest.mark.skipif(not is_gpu_available(), reason='No GPU')
+if is_gpu_available():
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+NOTEBOOKS_DIR = './notebooks/'
+NOTEBOOKS = glob(NOTEBOOKS_DIR + '*.ipynb')
+
+TUTORIALS_DIR = './../../examples/tutorials/'
+TUTORIALS = glob(TUTORIALS_DIR + '*.ipynb')
 ALLOWED_TUTORIALS = [
     '01',
     '02',   # quite long
@@ -15,23 +28,48 @@ ALLOWED_TUTORIALS = [
     # '10', # requires multiprocess module
 ]
 
+MICROBATCH_LIST = [None, 4]
+DEVICE_LIST = [None, pytest.param('GPU:*', marks=NO_GPU)]
+
+# Each parameter is (path, microbatch) configuration
 PARAMETERS = []
-PARAMETERS += [(path, mb) for path in glob('./notebooks/*.ipynb')
-               for mb in [None, 4]]
-PARAMETERS += [(path, None) for path in glob('./../../examples/tutorials/*.ipynb')
+
+# Run every notebook in test directory for every combination of microbatching
+PARAMETERS += [(path, mb) for path in NOTEBOOKS
+               for mb in MICROBATCH_LIST]
+
+# Run selected notebooks inside tutorials dir without microbatching
+PARAMETERS += [(path, None) for path in TUTORIALS
                if path.split('/')[-1][:2] in ALLOWED_TUTORIALS]
 
-print('\n', PARAMETERS, '\n')
+_ = [print(item) for item in PARAMETERS]
+
+
+BAD_PREFIXES = ['get_ipython', 'plt', 'plot', 'figure', 'ax.',]
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize('parameter', PARAMETERS)
-def test_run_notebooks(parameter):
-    """ There are a lot of examples in different notebooks, and all of them
-    should be working.
+@pytest.mark.parametrize('path, microbatch', PARAMETERS)
+@pytest.mark.parametrize('device', DEVICE_LIST)
+def test_run_notebooks(path, microbatch, device):
+    """ There are a lot of examples in different notebooks, and all of them should be working.
+
+    Parameters
+    ----------
+    path : str
+        Location of notebook to run.
+
+    microbatch : int or None
+        If None, then no microbatch is applied.
+        If int, then size of microbatch used.
+
+    device : str or None
+        If None, then default device behaviour is used.
+        If str, then any option of device configuration from :class:`.tf.TFModel` is supported.
     """
     # pylint: disable=exec-used
-    path, microbatch = parameter
+    if path.startswith(TUTORIALS_DIR) and device:
+        pytest.skip("Tutorials don't utilize device config.")
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
@@ -46,4 +84,4 @@ def test_run_notebooks(parameter):
                 code_.append(line)
 
     code = '\n'.join(code_)
-    exec(code, {'MICROBATCH': microbatch})
+    exec(code, {'MICROBATCH': microbatch, 'DEVICE': device})
