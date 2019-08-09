daba505b860d855324c99e39c61b4327a9a69821
a-arefina <7520522+a-arefina@users.noreply.github.com>
2019-08-09 17:14:58 +0300

    dataset ang config generation for img classification moved to conftest model_load_save_test v0

--- batchflow/tests/model_save_load_test.py
+++ batchflow/tests/model_save_load_test.py
@@ -1,43 +1,61 @@
 """ Test for model saving and loading """
 
 import pytest
-import numpy as np
 
-from batchflow import Pipeline, ImagesBatch, Dataset
+from batchflow import Pipeline
 from batchflow import B, V, C
 
 from batchflow.models.tf import VGG7 as TF_VGG7
 
 
-# TODO Extract to utilities
-# @pytest.fixture()
-def model_setup():
-    """ Pytest fixture to generate fake dataset and model config.
+PATH = 'my_mdl'
+BATCH_SIZE = 20
 
-    Returns
-    -------
-    tuple
-        an instance of Dataset
-        a model config
+save_pipeline = (Pipeline()
+                 .init_variable('predictions')
+                 .init_model('dynamic', C('model_class'), 'model', C('model_config'))
+                 .to_array()
+                 .predict_model('model', images=B('images'), labels=B('labels'),
+                                fetches='predictions', save_to=V('predictions', mode='w'))
+                 .save_model('model', path=PATH))
+
+load_pipeline = (Pipeline()
+                 .init_variable('predictions')
+                 .load_model('dynamic', C('model_class'), 'model', path=PATH)
+                 .to_array()
+                 .predict_model('model', images=B('images'), labels=B('labels'),
+                                fetches='predictions', save_to=V('predictions', mode='w'))
+                 )
+
+PIPELINES = [(load_pipeline, save_pipeline)]
+
+
+@pytest.mark.slow
+class TestModelSaveLoad:
+    """ Ensure that a model can be built and trained.
+
+    There is a following pattern in every test:
+        First of all, we get 'data' and 'config' via 'model_setup' fixture.
+        Then we optionally modify 'config'. In this case we modify only 'model' argument.
+        Finally, we assert that our modification was actually applied to a model by attempting
+        to build and train it with a small batch.
     """
-    def _model_setup():
-        size = 50
-        image_shape = (2, 100, 100)
-        batch_shape = (size, *image_shape)
-        images_array = np.random.random(batch_shape)
-        labels_array = np.random.choice(10, size=size)
-        data = images_array, labels_array
-        dataset = Dataset(index=size,
-                          batch_class=ImagesBatch,
-                          preloaded=data)
 
-        model_config = {'inputs/images/shape': image_shape,
-                        'inputs/labels/classes': 10,
-                        'initial_block/inputs': 'images'}
-        return dataset, model_config
+    @pytest.mark.parametrize('pipelines', PIPELINES)
+    def test_tf_model_load_save(self, pipelines, model_setup_images_clf):
+        """ We can explicitly pass 'data_format' to inputs or common."""
 
-    return _model_setup
+        dataset, model_config = model_setup_images_clf(image_shape=(100, 100, 2))
+        config = {'model_class': TF_VGG7, 'model_config': model_config}
 
-# TODO Create Pipeline, init model, predict on fake dataset, save predictions, save model
-# TODO load saved model, predict on same dataset, compare predictions to saved ones
-# TODO think about before/after/inside pipeline
+        load_pipeline, save_pipeline = pipelines
+
+        save_pipeline = (save_pipeline << dataset) << config
+        save_pipeline.run(BATCH_SIZE, n_epochs=1)
+        saved_predictions = save_pipeline.get_variable('predictions')
+
+        load_pipeline = (load_pipeline << dataset) << config
+        load_pipeline.run(BATCH_SIZE, n_epochs=1)
+        loaded_predictions = load_pipeline.get_variable('predictions')
+
+        assert (saved_predictions == loaded_predictions).all()
