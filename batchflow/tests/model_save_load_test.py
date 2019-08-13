e5ca3675890ecfb945515cbbab03cdaea8186cd7
a-arefina <7520522+a-arefina@users.noreply.github.com>
2019-08-13 14:58:37 +0300

    remove mode parametrization refactor save_path

--- batchflow/tests/model_save_load_test.py
+++ batchflow/tests/model_save_load_test.py
@@ -16,10 +16,6 @@ BATCH_SIZE = 20
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize(
-    'mode',
-    [pytest.param('static', marks=pytest.mark.skip(reason="doesn't work")),
-     'dynamic'])
 class TestModelSaveLoad:
     """
     Ensure that a model can be saved and loaded.
@@ -33,7 +29,7 @@ class TestModelSaveLoad:
         return str((tmp_path / PATH).absolute())
 
     @pytest.fixture
-    def tf_pipelines(self, model_setup_images_clf, mode):
+    def tf_pipelines(self, model_setup_images_clf):
         """
         Create pipelines with TF models to be saved and to be loaded
         Same dataset with no shuffling should be used, so that on same iterations
@@ -61,7 +57,7 @@ class TestModelSaveLoad:
         return save_pipeline, load_pipeline
 
     @pytest.fixture
-    def torch_pipelines(self, model_setup_images_clf, mode):
+    def torch_pipelines(self, model_setup_images_clf):
         """
         Create pipelines with Torch models to be saved and to be loaded
         see :meth:`~.TestModelSaveLoad.tf_pipelines` for details
@@ -96,34 +92,25 @@ class TestModelSaveLoad:
                              [pytest.param('tf_pipelines', (), dict(images=B('images'), labels=B('labels')),
                                            id="tf_model"),
                               pytest.param('torch_pipelines', (B('images'), B('labels')), dict(fetches='loss'),
-                                           id='torch_model')])
-    def test_run(self, fixture_dict, fixture_name, save_path, mode, model_args, model_kwargs):
+                                           id='torch')])
+    def test_run(self, fixture_dict, fixture_name, save_path, model_args, model_kwargs):
         """
         Check model loading and saving during pipeline iterations
 
         A model is initialised in save_pipeline, then for each batch:
             predictions are obtained and saved;
-            path for model saving is constructed using current iteration number;
             current model state is saved;
             the model is trained.
         After that in load_pipeline for each batch in same dataset:
-            path for model loading is constructed using current iteration number;
-            the model is loaded;
+            the model from corresponding iteration is loaded;
             predictions are obtained and saved.
 
         Predictions from save_pipeline and from load_pipeline should be equal
         """
         save_pipeline, load_pipeline = fixture_dict[fixture_name]
 
-        def make_path_name(batch, path, iteration):
-            _ = batch
-            return "{}_{}".format(path, iteration)
-
         save_tmpl = (Pipeline()
-                     .init_variable('save_path')
-                     .call(make_path_name, save_to=V("save_path"),
-                           kwargs=dict(path=save_path, iteration=I("current")))
-                     .save_model('model', path=V("save_path"))
+                     .save_model('model', path=save_path + I("current").str())
                      .train_model('model', *model_args, **model_kwargs))
 
         save_pipeline = save_pipeline + save_tmpl
@@ -131,10 +118,7 @@ class TestModelSaveLoad:
         saved_predictions = save_pipeline.get_variable('predictions')
 
         load_tmpl = (Pipeline()
-                     .init_variable('save_path')
-                     .call(make_path_name, save_to=V("save_path"),
-                           kwargs=dict(path=save_path, iteration=I("current")))
-                     .load_model(mode, C('model_class'), 'model', path=V("save_path")))
+                     .load_model('dynamic', C('model_class'), 'model', path=save_path + I("current").str()))
 
         load_pipeline = load_tmpl + load_pipeline
         load_pipeline.run(BATCH_SIZE, n_epochs=1, bar=True)
@@ -142,8 +126,12 @@ class TestModelSaveLoad:
 
         assert (np.concatenate(saved_predictions) == np.concatenate(loaded_predictions)).all()
 
-    @pytest.mark.parametrize('fixture_name', ['tf_pipelines', 'torch_pipelines'])
-    def test_now(self, fixture_dict, fixture_name, mode, save_path):
+    @pytest.mark.parametrize('fixture_name',
+                             [pytest.param('tf_pipelines',
+                                           id="tf"),
+                              pytest.param('torch_pipelines',
+                                           id='torch')])
+    def test_now(self, fixture_dict, fixture_name, save_path):
         """
         Test model loading and saving with `save_model_now`  and `load_model_now`
         """
@@ -153,14 +141,18 @@ class TestModelSaveLoad:
         saved_predictions = save_pipeline.get_variable('predictions')
         save_pipeline.save_model_now('model', path=save_path)
 
-        load_pipeline.load_model_now(mode, C('model_class'), 'model', path=save_path)
+        load_pipeline.load_model_now('dynamic', C('model_class'), 'model', path=save_path)
         load_pipeline.run(BATCH_SIZE, n_epochs=1)
         loaded_predictions = load_pipeline.get_variable('predictions')
 
         assert (np.concatenate(saved_predictions) == np.concatenate(loaded_predictions)).all()
 
-    @pytest.mark.parametrize('fixture_name', ['tf_pipelines', 'torch_pipelines'])
-    def test_after_before(self, fixture_dict, fixture_name, mode, save_path):
+    @pytest.mark.parametrize('fixture_name',
+                             [pytest.param('tf_pipelines',
+                                           id="tf"),
+                              pytest.param('torch_pipelines',
+                                           id='torch')])
+    def test_after_before(self, fixture_dict, fixture_name, save_path):
         """
         Test model saving in pipeline.after and loading in pipeline.before
         """
@@ -170,7 +162,7 @@ class TestModelSaveLoad:
         save_pipeline.run(BATCH_SIZE, n_epochs=1)
         saved_predictions = save_pipeline.get_variable('predictions')
 
-        load_pipeline.before.load_model(mode, C('model_class'), 'model', path=save_path)
+        load_pipeline.before.load_model('dynamic', C('model_class'), 'model', path=save_path)
         load_pipeline.run(BATCH_SIZE, n_epochs=1)
         loaded_predictions = load_pipeline.get_variable('predictions')
 
