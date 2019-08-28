31ac102576dd24647439e5577c1056c66dc4cf51
nikita <nikita_007_94@mail.ru>
2019-08-28 12:32:14 +0300

    avoid loop and copy

--- batchflow/opensets/small_imagenet.py
+++ batchflow/opensets/small_imagenet.py
@@ -20,7 +20,7 @@ from .. import DatasetIndex
 logger = logging.getLogger('SmallImagenet')
 
 
-class SmallImagenet(ImagesOpenset):
+class Imagenette(ImagesOpenset):
     """ Base class for ImagenetLike datasets
 
     Notes
@@ -29,7 +29,7 @@ class SmallImagenet(ImagesOpenset):
       Argument `drop_grayscale` controls whether grayscale images should be dropped.
 
     """
-    SOURCE_URL = None
+    SOURCE_URL = 'https://s3.amazonaws.com/fast-ai-imageclas/imagenette.tgz'
     num_classes = 10
 
     def __init__(self, *args, drop_grayscale=True, bar=False, preloaded=None, train_test=True, **kwargs):
@@ -52,19 +52,23 @@ class SmallImagenet(ImagesOpenset):
             """
             return basename(dirname(dirname(filepath))) == 'train'
 
-        def _extract(archive_file, member):
-            data = archive_file.extractfile(member).read()
+        def _extract(archive, member):
+            data = archive.extractfile(member).read()
             return PIL.Image.open(BytesIO(data))
+
+        def _is_file_rgb(archive, member):
+            """ Check whether archive member is a file.
+            In case `drop_grayscale` set to `True` it verifies that the member is the RGB mode image as well.
+            """
+            if not self.drop_grayscale:
+                return member.isfile()
+
+            return member.isfile() and _extract(archive, member).mode == 'RGB'
 
         def _gather_extracted(archive, files):
             images = np.array([_extract(archive, file) for file in files], dtype=object)
             labels = np.array([_image_class(file.name) for file in files])
             labels_encoded = LabelEncoder().fit_transform(labels)
-
-            if self.drop_grayscale:
-                rgb_mask = [i for i in range(len(images)) if images[i].mode == 'RGB']
-                return images[rgb_mask], labels_encoded[rgb_mask]
-
             return images, labels_encoded
 
         if path is None:
@@ -83,10 +87,10 @@ class SmallImagenet(ImagesOpenset):
         with tarfile.open(localname, "r:gz") as archive:
             files_in_archive = archive.getmembers()
 
-            train_files = [file for file in files_in_archive if file.isfile() and _is_train(file.name)]
+            train_files = [file for file in files_in_archive if _is_file_rgb(archive, file) and _is_train(file.name)]
             train_data = _gather_extracted(archive, train_files)
 
-            test_files = [file for file in files_in_archive if file.isfile() and not _is_train(file.name)]
+            test_files = [file for file in files_in_archive if _is_file_rgb(archive, file) and not _is_train(file.name)]
             test_data = _gather_extracted(archive, test_files)
 
         logger.info("Extracted")
@@ -98,40 +102,34 @@ class SmallImagenet(ImagesOpenset):
         return train_data, test_data
 
 
-class Imagenette(SmallImagenet):
-    """ Imagenette dataset. See the https://github.com/fastai/imagenette for details.
-    """
-    SOURCE_URL = 'https://s3.amazonaws.com/fast-ai-imageclas/imagenette.tgz'
-
-
-class Imagenette320(SmallImagenet):
+class Imagenette320(Imagenette):
     """ The '320px' version of Imagenette.
     The shortest size resized to that size with their aspect ratio maintained.
     """
     SOURCE_URL = 'https://s3.amazonaws.com/fast-ai-imageclas/imagenette-320.tgz'
 
 
-class Imagenette160(SmallImagenet):
+class Imagenette160(Imagenette):
     """ The '160px' version of Imagenette.
     The shortest size resized to that size with their aspect ratio maintained.
     """
     SOURCE_URL = 'https://s3.amazonaws.com/fast-ai-imageclas/imagenette-160.tgz'
 
 
-class ImageWoof(SmallImagenet):
+class ImageWoof(Imagenette):
     """ Imagewoof dataset. See the https://github.com/fastai/imagenette for details.
     """
     SOURCE_URL = 'https://s3.amazonaws.com/fast-ai-imageclas/imagewoof.tgz'
 
 
-class ImageWoof320(SmallImagenet):
+class ImageWoof320(Imagenette):
     """ The '320px' version of Imagewoof.
     The shortest size resized to that size with their aspect ratio maintained.
     """
     SOURCE_URL = 'https://s3.amazonaws.com/fast-ai-imageclas/imagewoof-320.tgz'
 
 
-class ImageWoof160(SmallImagenet):
+class ImageWoof160(Imagenette):
     """ The '160px' version of Imagewoof.
     The shortest size resized to that size with their aspect ratio maintained.
     """
