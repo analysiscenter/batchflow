b101396c70fa77b6707cb45e21e6c8e61d7a1f5d
Dmitry Zenkov <dimonovez@gmail.com>
2019-09-05 16:20:47 +0300

    Add aliases and  disable pylint

--- batchflow/models/tf/nn/train.py
+++ batchflow/models/tf/nn/train.py
@@ -2,17 +2,16 @@
 from math import pi
 
 import tensorflow as tf
-from tensorflow.python.framework import ops
-from tensorflow.python.ops import math_ops
-# from tensorflow.python.keras.optimizer_v2.learning_rate_schedule import LearningRateSchedule
-from tensorflow.math import sin, asin, floor
+from tensorflow.python.framework import ops # pylint: disable=no-name-in-module
+from tensorflow.python.ops import math_ops # pylint: disable=no-name-in-module
+from tensorflow.math import sin, asin, floor # pylint: disable=import-error
 
 
 def piecewise_constant(global_step, *args, **kwargs):
     """ Constant learning rate decay (uses global_step param instead of x) """
     return tf.train.piecewise_constant(global_step, *args, **kwargs)
 
-def cyclic_learning_rate(global_step, learning_rate=0.05, max_lr=0.1, step_size=10, mode='sin', name=None):
+def cyclic_learning_rate(learning_rate, global_step, max_lr=0.1, step_size=10, mode='sin', name=None):
 
     with ops.name_scope_v2(name or "CyclicLearningRate") as name:
         learning_rate = ops.convert_to_tensor(learning_rate, name="learning_rate")
@@ -20,16 +19,16 @@ def cyclic_learning_rate(global_step, learning_rate=0.05, max_lr=0.1, step_size=
         global_step = math_ops.cast(global_step, dtype)
         step_size = math_ops.cast(step_size, dtype)
 
-        if mode == 'sin':
+        if mode in ('sin', 'sine', 'sine wave'):
         # (max_lr-learning_rate)/2 * sin(pi*global_step/step_size - pi) + (max_lr+learning_rate)/2
             first_factor = math_ops.divide(math_ops.subtract(learning_rate, max_lr), 2.)
             second_factor = sin(math_ops.divide(math_ops.multiply(pi, global_step), step_size))
-        elif mode == 'triangular':
+        elif mode in ('triangular', 'triangular wave', 'zigzag'):
         # ((max_lr-learning_rate)/pi) * asin(sin(((2*-pi)/step_size)*global_step)) + (max_lr+learning_rate)/2
             first_factor = math_ops.divide(math_ops.subtract(max_lr, learning_rate), pi)
             inside_sin = math_ops.multiply(math_ops.divide(math_ops.multiply(2., -pi), step_size), global_step)
             second_factor = asin(sin(inside_sin))
-        elif mode == 'sawtooth':
+        elif mode in ('sawtooth', 'saw', 'sawtooth wave', 'saw wave'):
         # (max_lr-learning_rate) * (global_step / step_size - floor(global_step / step_size)) + learning_rate
             first_factor = math_ops.subtract(max_lr, learning_rate)
             divided_global_step = math_ops.divide(global_step, step_size)
