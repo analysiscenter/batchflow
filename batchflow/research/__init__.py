62aeb7595c51e932911ff3306ab4905e9214d116
alexeykozhevin <a.kozhevin@analysiscenter.org>
2019-10-29 12:41:00 +0300

    Add Telegram logger

--- batchflow/research/__init__.py
+++ batchflow/research/__init__.py
@@ -1,7 +1,7 @@
 """ Research module. """
 from .domain import KV, Domain, Option, ConfigAlias
 from .distributor import Distributor
-from .logger import Logger, BasicLogger
+from .logger import Logger, BasicLogger, PrintLogger, TelegramLogger
 from .workers import Worker, PipelineWorker
 from .named_expr import ResearchNamedExpression, ResearchExecutableUnit, ResearchPipeline, \
                         ResearchIteration, ResearchConfig, ResearchResults, ResearchPath
--- batchflow/research/logger.py
+++ batchflow/research/logger.py
@@ -3,17 +3,36 @@
 import os
 import traceback
 import logging
+import requests
 
 from .named_expr import ResearchPath
 from ..named_expr import eval_expr
+
+def _get_traceback(exception):
+    ex_traceback = exception.__traceback__
+    return ''.join(traceback.format_exception(exception.__class__, exception, ex_traceback))
+
+def log_info(message, path):
+    """ Write message into log. """
+    filename = os.path.join(path, 'research.log')
+    logging.basicConfig(format='%(levelname)-8s [%(asctime)s] %(message)s', filename=filename, level=logging.INFO)
+    logging.info(message)
+
+def log_error(exception, path):
+    """ Write error message into log. """
+    filename = os.path.join(path, 'research.log')
+    logging.basicConfig(format='%(levelname)-8s [%(asctime)s] %(message)s', filename=filename, level=logging.ERROR)
+    logging.info(_get_traceback(exception))
 
 class Logger:
     """ Basic logging class.
 
     Logger consists of one or few pairs of functions (info logging and error logging).
     """
-    def __init__(self):
-        self._loggers = []
+    def __init__(self, loggers=None):
+        if loggers is None:
+            loggers = []
+        self._loggers = loggers
 
     def info(self, message, **kwargs):
         """ Log some message
@@ -26,7 +45,8 @@ class Logger:
             parameters for info function
         """
         for item in self._loggers:
-            item['info'](message, **item['kwargs'], **kwargs)
+            if 'info' in item and item['info'] is not None:
+                item['info'](message, **item['kwargs'], **kwargs)
 
     def error(self, exception, **kwargs):
         """ Log some exception
@@ -41,8 +61,6 @@ class Logger:
         for item in self._loggers:
             if 'error' in item and item['error'] is not None:
                 item['error'](exception, **item['kwargs'], **kwargs)
-            else:
-                item['info'](exception, **item['kwargs'], **kwargs)
 
     def append(self, info, error=None, **kwargs):
         self._loggers.append({
@@ -55,22 +73,49 @@ class Logger:
         for item in self._loggers:
             item['kwargs'] = eval_expr(item['kwargs'], **kwargs)
 
+    def __add___(self, other):
+        if isinstance(other, Logger):
+            return Logger(self._loggers + other._logers)
+        else:
+            raise TypeError("unsupported operand type(s) for +: '{}' and '{}'".format(type(self), type(other)))
+
 class BasicLogger(Logger):
     """ Basic logging class """
     def __init__(self):
         super().__init__()
         self._loggers = [{'info': log_info, 'error': log_error, 'kwargs': dict(path=ResearchPath())}]
 
-def log_info(message, path):
-    """ Write message into log. """
-    filename = os.path.join(path, 'research.log')
-    logging.basicConfig(format='%(levelname)-8s [%(asctime)s] %(message)s', filename=filename, level=logging.INFO)
-    logging.info(message)
+class PrintLogger(Logger):
+    """ Logging by print """
+    def __init__(self):
+        super().__init__()
+        self._loggers = [{'info': print, 'error': print}]
 
-def log_error(exception, path):
-    """ Write error message into log. """
-    filename = os.path.join(path, 'research.log')
-    logging.basicConfig(format='%(levelname)-8s [%(asctime)s] %(message)s', filename=filename, level=logging.INFO)
-    ex_traceback = exception.__traceback__
-    tb_lines = ''.join(traceback.format_exception(exception.__class__, exception, ex_traceback))
-    logging.info(tb_lines)
+class TelegramLogger(Logger):
+    """ Telegram Logger """
+    def __init__(self, bot_token, chat_id):
+        """ Initialize Logger
+
+        Parameters
+        ----------
+        bot_token : str
+            telegram bot token
+
+        chat_id : int or str
+
+        **How to get token and chat id**
+            See https://github.com/datagym-ru/tg_tqdm/
+        """
+        super().__init__()
+        self.bot_token = bot_token
+        self.chat_id = str(chat_id)
+        self._loggers = [{'info': self._info, 'error': self._info, 'kwargs': dict()}]
+
+    def _info(self, message):
+        send_text = ('https://api.telegram.org/bot' + self.bot_token + '/sendMessage?chat_id='
+                     + self.chat_id + '&parse_mode=Markdown&text=' + message)
+        response = requests.get(send_text)
+        return response.json()
+
+    def _error(self, exception):
+        return self._info(_get_traceback(exception))
