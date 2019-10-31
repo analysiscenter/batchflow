a901232833630ffa90671716563a98282993aacb
alexeykozhevin <a.kozhevin@analysiscenter.org>
2019-10-31 18:11:36 +0300

    Workers will not share devices if possible

--- batchflow/research/research.py
+++ batchflow/research/research.py
@@ -379,8 +379,8 @@ class Research:
             devices = [devices]
         if isinstance(devices[0], (int, str)):
             if total_n_branches > len(devices):
-
-                devices = devices * (total_n_branches // len(devices)) + devices[:total_n_branches % len(devices)]
+                _devices = list(itertools.chain.from_iterable(zip(*itertools.repeat(devices, total_n_branches // len(devices)))))
+                devices = _devices + devices[:total_n_branches % len(devices)]
             else:
                 devices = devices + devices[:-len(devices) % (total_n_branches)]
             if total_n_branches % len(devices) == 0:
