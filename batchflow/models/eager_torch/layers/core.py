12f26f43b068a58a7783b3b86198b3ec61c029f9
nikita <nikita_007_94@mail.ru>
2019-11-15 19:32:17 +0300

    initial commit for multisample dropout

--- batchflow/models/eager_torch/layers/core.py
+++ batchflow/models/eager_torch/layers/core.py
@@ -2,8 +2,10 @@
 import inspect
 
 import numpy as np
+import torch
 import torch.nn as nn
 
+from .resize import Combine
 from ..utils import get_shape, get_num_channels, get_num_dims
 
 
@@ -128,12 +130,44 @@ class Dropout(nn.Module):
         3: nn.Dropout3d,
     }
 
-    def __init__(self, inputs=None, dropout_rate=0.0, **kwargs):
+    def __init__(self, inputs=None, dropout_rate=0.5, multisample=False, **kwargs):
         super().__init__()
-        self.layer = self.LAYERS[get_num_dims(inputs)](p=dropout_rate, **kwargs)
+        self.multisample = multisample
+        self.sum = Combine(op='sum')
+        print(dropout_rate, multisample)
+        self.layer = self.LAYERS[get_num_dims(inputs)](p=dropout_rate)
 
     def forward(self, x):
-        return self.layer(x)
+        if self.multisample is not False:
+            if self.multisample is True:
+                self.multisample = 2
+            elif isinstance(self.multisample, float):
+                self.multisample = [self.multisample, 1 - self.multisample]
+
+            if isinstance(self.multisample, int): # dropout to the whole batch, then average
+                dropped = [self.layer(x) for _ in range(self.multisample)]
+                output = self.sum(dropped) / self.multisample
+            else: # split batch into separate-dropout branches
+                if isinstance(self.multisample, (tuple, list)):
+                    if all([isinstance(item, int) for item in self.multisample]):
+                        sizes = self.multisample
+                    elif all([isinstance(item, float) for item in self.multisample]):
+                        batch_size = x.shape[0]
+                        sizes = torch.Tensor([batch_size*item for item in self.multisample[:-1]])
+                        sizes = torch.round(sizes)
+                        residual = (x.shape[0] - torch.mean(sizes))
+                        residual = torch.reshape(residual, shape=(1,))
+                        sizes = torch.cat([sizes, residual], axis=0).to(int)
+                else: # case of Tensor
+                    sizes = self.multisample
+
+                splitted = torch.split(x, tuple(sizes))
+                dropped = [self.layer(branch) for branch in splitted]
+                output = torch.cat(dropped, axis=0)
+        else:
+            output = self.layer(x)
+        return output
+
 
 class AlphaDropout(Dropout):
     """ Multi-dimensional alpha-dropout layer. """
