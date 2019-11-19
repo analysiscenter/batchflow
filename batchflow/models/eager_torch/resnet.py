c15b97458069ea19aa0def4259264ae0c2b32644
Dmitry Zenkov <dimonovez@gmail.com>
2019-11-19 14:50:12 +0300

    Change resblock behavior

--- batchflow/models/eager_torch/resnet.py
+++ batchflow/models/eager_torch/resnet.py
@@ -1,39 +1,46 @@
 """."""
+from copy import deepcopy
 import numpy as np
 import torch.nn as nn
 
 from .layers import ConvBlock
 from .base import EagerTorch
 
+CONV_LETTERS = ['c', 'C', 'w', 'W', 't', 'T']
+
 class ResBlock(nn.Module):
     """ Add modules """
-    def __init__(self, inputs, layout, filter, downsample, bottleneck, resnext):
+    def __init__(self, layout='cnacna', filters=64, downsample=False, bottleneck=False, groups=1):
         super().__init__()
 
-        conv_letters = ['c', 'C', 'w', 'W', 't', 'T']
-        num_convs = sum([letter in conv_letters for letter in layout])
+        num_convs = sum([letter in CONV_LETTERS for letter in layout])
 
         self.layout = layout
-        self.kernel_size = [3] * num_convs
-        self.filters = [filter] * num_convs
+        self.filters = [filter] * num_convs if isinstance(filters, int) else filters
         self.strides = [1] * num_convs
+        self.kernel_size = [3] * num_convs
+        self.groups = groups * num_convs
 
         if downsample:
             self.strides = [2] + self.strides[1:]
         if bottleneck:
-            self.kernel_size = [1] + self.kernel_size[1:-1] + [1]
-            self.filters = self.filters[:-1] + [filter * 4]
-        if resnext:
-            self.groups = resnext
-        else:
-            self.groups = 1
+            self.layout = 'cna' + self.layout + 'cna'
+            self.kernel_size = [1] + self.kernel_size + [1]
+            self.strides = [1] + self.strides + [1]
+            if boottleneck is True:
+                boottleneck = 4
+            self.filters = [self.filters[0]] + self.filters + [self.filters[0] * bottleneck]
+            self.groups = [1, self.groups, 1]
 
+
+        self.main_branch = ConvBlock(layout=self.layout, filters=self.filters,
+                                     kernel_size=self.kernel_size, strides=self.strides, groups=self.groups)
+        self.side_branch = ConvBlock(layout='c', filters=self.filters[-1],
+                                     kernel_size=1, strides=2)
 
     def forward(self, x):
         """ Make a forward pass """
-        x = self.layer = ConvBlock(inputs=x, layout=self.layout, filters=self.filters,
-                               kernel_size=self.kernel_size, strides=self.strides, groups=self.groups)
-        return x
+        return self.main_branch(x) + self.side_branch(x)
 
 
 class ResNet(EagerTorch):
@@ -46,7 +53,8 @@ class ResNet(EagerTorch):
         config['initial_block'] += dict(layout='cnap', filters=64, kernel_size=7, strides=2,
                                         pool_size=3, pool_strides=2)
 
-        config['body/block'] = dict(layout='R cna cna +', bottleneck=False, resnext=False)
+        config['body/block'] = dict(layout='cna cna', filters=None,
+                                    downsample=None, bottleneck=False, groups=1)
         config['body/width_factor'] = 1
 
         # config['head'] += dict(layout='Vdf', dropout_rate=.4)
@@ -72,6 +80,7 @@ class ResNet(EagerTorch):
             num_blocks = config['body/num_blocks']
             config['body/downsample'] = [False] + [True] * (len(num_blocks) - 1)
 
+
         # if config.get('head/units') is None:
         #     config['head/units'] = self.num_classes('targets')
         # if config.get('head/filters') is None:
@@ -84,21 +93,19 @@ class ResNet(EagerTorch):
         kwargs = cls.get_defaults('body', kwargs)
         num_blocks = kwargs.get('num_blocks')
         filters = kwargs.get('filters')
-        downsample_body = kwargs.get('downsample')
-        list_layers = []
+        downsample_layer = kwargs.get('downsample')
+        # list_layers = []
 
-        block_params = kwargs['block']
-        x = inputs
-        print(kwargs)
-        for num_block, filter, downsample_block in zip(num_blocks, filters, downsample_body):
-            downsample_block = [downsample_block] + [False] * (num_block - 1)
-            for block, downsample in zip(range(num_block), downsample_block):
-                # block_params['downsample'] = downsample
-                # block_params['filter'] = filter
-                # print(downsample, filter, dict(block_params))
-                x = ResBlock(inputs=x, downsample=downsample, filter=filter, **block_params)
-                print(x)
-                # layout, filter, downsample, bottleneck, resnext):
+        resblock_args = deepcopy(kwargs['block'])
+        # x = inputs
+        for num_resblocks, filter, downsample in zip(num_blocks, filters, downsample_layer):
+            for idx_resblock in range(num_resblocks):
+                downsample = downsample if idx_resblock == 0 else False
+                resblock_args.update({'filters': filter, 'downsample': downsample})
+                print(resblock_args)
+
+                # x = ResBlock(**resblock_args)
+
                 downsample = False
         # kwargs = cls.get_defaults('body', kwargs)
         # filters, block_args = cls.pop(['filters', 'block'], kwargs)
