3ae990e53fa4d2681d56225fb9c463d14d4f25cb
Stepan Goryachev <53620809+cdtn@users.noreply.github.com>
2019-12-19 15:25:27 +0300

    Add MobileNet model

---
+++ batchflow/models/torch/mobilenet.py
@@ -0,0 +1,25 @@
+"""  Howard A. et al "`MobileNets: Efficient Convolutional Neural Networks for Mobile Vision Applications
+<https://arxiv.org/abs/1704.04861>`_"
+"""
+from .encoder_decoder import Encoder
+from .blocks import MobileBlock
+
+MOBILENET_V1_ENCODER_CONFIG = dict(strides=[1, 2, 1, 2, 1, 2, 1, 2, 2],
+                                   downsample=[True, True, False, True, False, True, False, True, False],
+                                   n_reps=[1, 1, 1, 1, 1, 1, 5, 1, 1])
+
+class MobileNet(Encoder):
+    @classmethod
+    def default_config(cls):
+        config = super().default_config()
+
+        config['initial_block'] += dict(layout='cna', filters=32, kernel_size=3, strides=2)
+
+        config['body/encoder/num_stages'] = 9
+        config['body/encoder/order'] = ['block']
+        config['body/encoder/blocks'] += dict(base=MobileBlock, **MOBILENET_V1_ENCODER_CONFIG)
+
+        config['head'] += dict(layout='Vf')
+
+        config['loss'] = 'ce'
+        return config
