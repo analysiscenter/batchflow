48f68c9af7a2b597132efc80dc50c6f820c25c3a
user <nikita_007_94@mail.ru>
2020-02-18 15:04:08 +0300

    Add docs for tests

--- batchflow/tests/pipeline_init_model_test.py
+++ batchflow/tests/pipeline_init_model_test.py
@@ -1,3 +1,5 @@
+""" Tests for pipeline init_model action """
+# pylint: disable=import-error, no-name-in-module
 import pytest 
 
 from batchflow import DatasetIndex, Pipeline, Batch, C
@@ -6,6 +8,7 @@ from batchflow.model_dir import NonInitializedModel
 
 @pytest.fixture
 def dataset():
+    """ Toy dataset """
     index = DatasetIndex(100)
     return Dataset(index, Batch)
 
@@ -13,6 +16,9 @@ def dataset():
 @pytest.mark.parametrize('config', [dict(mode='static', base_class=BaseModel, name='model', config={}),
                                     dict(mode='dynamic', base_class=BaseModel, name='model', config={})])
 def test_single_model(use_algebra, config, dataset):
+    """ Verifies that the model in init_model is initialized as expected with config provided, i.e.
+    pipeline contains the model with given name belongs to given class.
+    Also veriying methods of linking config to the pipeline: via pipeline algebra and manually. """
     if not use_algebra:
         pipeline = dataset.pipeline(config).init_model(C('mode'), C('base_class'), C('name'), C('config'))
     else:
@@ -24,24 +30,26 @@ def test_single_model(use_algebra, config, dataset):
     else:
         assert type(pipeline.m(config['name']) == NonInitializedModel
 
-# @pytest.mark.parametrize('use_algebra1', [True, False])
-# @pytest.mark.parametrize('use_algebra2', [True, False])
-# @pytest.mark.parametrize('mode', ['static', 'dynamic'])
-# @pytest.mark.parametrize('config1', 'config2' [(dict(class=BaseModel, name='model1', config={}),
-#                                                 dict(class=BaseModel, name='model2', config={})])
-# def test_multiple_models(dataset, use_algebra1, use_algebra2, mode,  config1, config2):
-#     config1.update(mode=mode)
-#     if not use_algebra1:
-#         pipeline = dataset.pipeline(config1).init_model(C('mode'), C('class'), C('name'), C('config'))
-#     else:
-#         pipeline = dataset.p.init_model(C('mode'), C('class'), C('name'), C('config')) << config1
-#     pipeline.next_batch(1)
+@pytest.mark.parametrize('use_algebra1', [True, False])
+@pytest.mark.parametrize('use_algebra2', [True, False])
+@pytest.mark.parametrize('mode', ['static', 'dynamic'])
+@pytest.mark.parametrize('config1', 'config2' [(dict(class=BaseModel, name='model1', config={}),
+                                                dict(class=BaseModel, name='model2', config={})])
+def test_multiple_models(dataset, use_algebra1, use_algebra2, mode,  config1, config2):
+    """ Verifies that several modeles can be initialized and stored in the pipeline via sequentially linking
+    different configs to the pipeline. """
+    config1.update(mode=mode)
+    if not use_algebra1:
+        pipeline = dataset.pipeline(config1).init_model(C('mode'), C('class'), C('name'), C('config'))
+    else:
+        pipeline = dataset.p.init_model(C('mode'), C('class'), C('name'), C('config')) << config1
+    pipeline.next_batch(1)
 
-#     config2.update(mode=mode)
-#     if not use_algebra2:
-#         pipeline = pipeline.update_config(config2)
-#     else:
-#         pipeline = pipeline << config2
-#     pipeline.next_batch(1)
+    config2.update(mode=mode)
+    if not use_algebra2:
+        pipeline = pipeline.update_config(config2)
+    else:
+        pipeline = pipeline << config2
+    pipeline.next_batch(1)
 
-#     assert type(pipeline.m(config1['name']) == type(pipeline.m(config2['name'])
+    assert type(pipeline.m(config1['name']) == type(pipeline.m(config2['name'])
