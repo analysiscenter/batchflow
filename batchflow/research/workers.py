c2b52ecb0036edf410acd5b8a0709fb09e5205d9
a-arefina <7520522+a-arefina@users.noreply.github.com>
2020-04-02 08:22:01 +0000

    fix pylint

--- batchflow/research/workers.py
+++ batchflow/research/workers.py
@@ -174,8 +174,8 @@ class PipelineWorker(Worker):
         """ Run before job execution. """
         i, job = self.job
         n_branches = len(job.configs)
-        all_devices = set([device for i in range(n_branches)
-                           for device in self.devices[i]['device'] if device is not None])
+        all_devices = set(device for i in range(n_branches)
+                          for device in self.devices[i]['device'] if device is not None)
 
         if len(all_devices) > 0:
             os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
