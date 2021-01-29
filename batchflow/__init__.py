f2ca2441e69fbddd0a1c3a3fa5a0590690143efb
Roman Kh <rhudor@gmail.com>
2021-01-29 13:50:46 +0300

    Add utils funcs to docs

--- batchflow/__init__.py
+++ batchflow/__init__.py
@@ -18,7 +18,7 @@ from .dsindex import DatasetIndex, FilesIndex
 from .decorators import action, inbatch_parallel, parallel, any_action_failed, mjit, deprecated, apply_parallel
 from .exceptions import SkipBatchException, EmptyBatchSequence
 from .sampler import Sampler, ConstantSampler, NumpySampler, HistoSampler, ScipySampler
-from .utils import save_data_to
+from .utils import save_data_to, read_data_from
 from .utils_notebook import in_notebook, get_notebook_path, get_notebook_name, pylint_notebook
 
 
