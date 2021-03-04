9ada2b4ccd1f5374cde3e29a45d04924588a03f3
Stepan Goryachev <53620809+cdtn@users.noreply.github.com>
2021-03-04 16:03:44 +0300

    Update version

--- batchflow/__init__.py
+++ batchflow/__init__.py
@@ -22,4 +22,4 @@ from .utils import save_data_to, read_data_from
 from .utils_notebook import in_notebook, get_notebook_path, get_notebook_name, pylint_notebook
 
 
-__version__ = '0.3.0'
+__version__ = '0.5.0beta'
