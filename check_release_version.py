0343cf5f2291331fc18c9697d2a182c391885614
Stepan Goryachev <53620809+cdtn@users.noreply.github.com>
2021-03-04 14:24:11 +0300

    Update check_release_version.py

--- check_release_version.py
+++ check_release_version.py
@@ -2,5 +2,5 @@ import re
 import sys
 
 with open('batchflow/__init__.py', 'r') as f:
-    version = re.search(r'^__version__\s*=\s*[\'"]v([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+    version = "v" + re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 sys.exit(version != sys.argv[1])
