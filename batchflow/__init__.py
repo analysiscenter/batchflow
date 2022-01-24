d059033b52b8736107c2187780dd37cf0b009d5a
HollowPrincess <risahawkeye@mail.ru>
2022-01-24 15:15:07 +0300

    little prettifyings

--- batchflow/__init__.py
+++ batchflow/__init__.py
@@ -17,10 +17,11 @@ from .named_expr import NamedExpression, B, L, C, F, V, M, D, R, W, P, PP, I, ev
 from .dsindex import DatasetIndex, FilesIndex
 from .decorators import action, inbatch_parallel, parallel, any_action_failed, mjit, deprecated, apply_parallel
 from .exceptions import SkipBatchException, EmptyBatchSequence, StopPipeline
+from .run_notebook import run_notebook
 from .sampler import Sampler, ConstantSampler, NumpySampler, HistoSampler, ScipySampler
 from .utils import save_data_to, read_data_from
 from .utils_random import make_rng, make_seed_sequence, spawn_seed_sequence
-from .utils_notebook import in_notebook, get_notebook_path, get_notebook_name, run_notebook, pylint_notebook,\
+from .utils_notebook import in_notebook, get_notebook_path, get_notebook_name, pylint_notebook,\
                             get_available_gpus, set_gpus
 from .utils_telegram import TelegramMessage
 
