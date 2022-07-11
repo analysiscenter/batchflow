b9612331d941ed0b6a15b2a53a1c3661facd69b4
Anastasiia-prog <anastasiia.41297@gmail.com>
2022-07-11 14:10:44 +0300

    fixed spaces

--- batchflow/models/torch/benchmark.py
+++ batchflow/models/torch/benchmark.py
@@ -1,3 +1,4 @@
+""" Time and memory trackers for benchmark operations with nn.Module. """
 import numpy as np
 import torch
 from ptflops import get_model_complexity_info
@@ -7,13 +8,17 @@ from .utils import make_initialization_inputs
 # Different units for memory representation
 MEMORY_UNIT_CONSTANTS = {
     'GB': 1 / (1024 ** 3),
-    'MB': 1 / (1024 ** 2), 
-    'KB': 1 / 1024, 
+    'MB': 1 / (1024 ** 2),
+    'KB': 1 / 1024,
     'B': 1.
 }
 
 class TimeTracker:
     """ Measure time taken for an operation on GPU. """
+    def __init__(self):
+        self.start = None
+        self.end = None
+
     def __enter__(self):
         self.start = torch.cuda.Event(enable_timing=True)
         self.end = torch.cuda.Event(enable_timing=True)
@@ -23,7 +28,7 @@ class TimeTracker:
     def __exit__(self, type, value, traceback):
         self.end.record()
         torch.cuda.synchronize()
-        
+
     @property
     def value(self):
         """ Get an operation time. """
@@ -33,25 +38,27 @@ class MemoryTracker:
     """ Measure peak used memory for an operation on GPU. """
     def __init__(self, device=None):
         self.device = device
-        
+        self.start_memory = None
+        self.end_memory = None
+
     def __enter__(self):
         torch.cuda.reset_peak_memory_stats()
         self.start_memory = torch.cuda.max_memory_allocated(self.device)
         return self
 
     def __exit__(self, type, value, traceback):
-        self.end_memory = torch.cuda.max_memory_allocated(self.device) 
-        
+        self.end_memory = torch.cuda.max_memory_allocated(self.device)
+
     @property
     def value(self):
         """ Get allocated memory for a module. """
-        return self.end_memory - self.start_memory 
+        return self.end_memory - self.start_memory
 
 def get_module_performance(module, inputs, n_repeats=300, warmup=40, device=None, track_backward=True,
                            channels_last=False, amp=False, memory_unit='MB'):
-    """ Track module #macs, #parameters, time and memory consumption on forward and backward 
+    """ Track module #macs, #parameters, time and memory consumption on forward and backward
     pass for a given input tensor or inputs shape.
-    
+
     Parameters
     ----------
     module : nn.Module
@@ -75,7 +82,7 @@ def get_module_performance(module, inputs, n_repeats=300, warmup=40, device=None
     memory_unit : str
         Memory units that are used for memory representation in the result.
         Possible options are: 'GB', 'MB', 'KB', 'B'.
-    
+
     Returns
     -------
     dict
@@ -88,7 +95,7 @@ def get_module_performance(module, inputs, n_repeats=300, warmup=40, device=None
             - total time for forward and backward together.
     """
     memory_unit_constant = MEMORY_UNIT_CONSTANTS[memory_unit]
-    
+
     with TimeTracker() as total_timer:
         result = {}
         forward_timings = []
@@ -101,7 +108,7 @@ def get_module_performance(module, inputs, n_repeats=300, warmup=40, device=None
 
         if channels_last:
             inputs.to(memory_format=torch.channels_last)
-            module.to(memory_format=torch.channels_last)          
+            module.to(memory_format=torch.channels_last)
 
         for i in range(n_repeats + warmup):
             if i < warmup:
@@ -112,22 +119,22 @@ def get_module_performance(module, inputs, n_repeats=300, warmup=40, device=None
                 continue
 
             with torch.cuda.amp.autocast(enabled=amp):
-                # Calculate forward operation time  
+                # Calculate forward operation time
                 with TimeTracker() as forward_timer:
                     outputs = module(inputs)
 
-                forward_time = forward_timer.value        
-                forward_timings.append(forward_time) 
+                forward_time = forward_timer.value
+                forward_timings.append(forward_time)
 
             if track_backward:
-                # Calculate backward operation time 
+                # Calculate backward operation time
                 with TimeTracker() as backward_timer:
                     outputs.backward(outputs)
  
                 backward_time = backward_timer.value
                 backward_timings.append(backward_time)
 
-        result['forward time mean(ms)'] = np.mean(forward_timings) 
+        result['forward time mean(ms)'] = np.mean(forward_timings)
         result['forward time std(ms)'] = np.std(forward_timings)
 
         # Calculate forward memory
@@ -148,7 +155,7 @@ def get_module_performance(module, inputs, n_repeats=300, warmup=40, device=None
             backward_memory = memory.value
             result['backward memory'] = backward_memory * memory_unit_constant
 
-        macs, params = get_model_complexity_info(module, tuple(inputs.shape[1:]), 
+        macs, params = get_model_complexity_info(module, tuple(inputs.shape[1:]),
                                                  as_strings=False, print_per_layer_stat=False)
         result['macs'] = macs
         result['parameters'] = float(params)
