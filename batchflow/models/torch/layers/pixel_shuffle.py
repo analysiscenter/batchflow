7d062677f92054423f875ac7b3fe9f3b6182c9cf
AnnaAltynova <altynanke@gmail.com>
2022-07-13 13:46:31 +0300

    pixel shuffle pylint fix

--- batchflow/models/torch/layers/pixel_shuffle.py
+++ batchflow/models/torch/layers/pixel_shuffle.py
@@ -1,5 +1,6 @@
 """ Layer implementing Pixel Shuffle for an arbitrary dimensionality.
-Shi et al. "`Real-Time Single Image and Video Super-Resolution Using an Efficient Sub-Pixel Convolutional Neural Network_ 
+Shi et al. "`Real-Time Single Image and Video Super-Resolution
+Using an Efficient Sub-Pixel Convolutional Neural Network_
 <https://arxiv.org/pdf/1609.05158.pdf>`"
 """
 from torch import nn
@@ -7,9 +8,10 @@ from torch import nn
 
 
 class PixelShuffle(nn.Module):
-    """ Rearranges elements in a tensor of shape [B, C * r^N, D1, D2, ... DN] to a tensor of shape [B, C, D1 * r, D2 * r, ... DN * r]
+    """ Rearranges elements in a tensor of shape [B, C * r^N, D1, D2, ... DN]
+    to a tensor of shape [B, C, D1 * r, D2 * r, ... DN * r]
     where r is an upscale factor.
-    
+
     Parameters
     ----------
     upscale_factor : int
@@ -23,7 +25,7 @@ class PixelShuffle(nn.Module):
         batch_size, channels, *dims = x.size()                                         # (B, C * r^N, H, W)
         ndims = len(dims)
         channels //= (self.upscale_factor**ndims)
-        
+
         factor_expand = [self.upscale_factor] * ndims
         x = x.contiguous().view(                                                       # (B, C, r, r, H, W)
             batch_size, channels, *factor_expand, *dims
@@ -44,18 +46,18 @@ class PixelShuffle(nn.Module):
 
 
 class PixelUnshuffle(nn.Module):
-    """ Reverses `PixelShuffle` operation by rearranging elements in a tensor of shape [B, C, D1 * r, D2 * r, ... DN * r]
+    """ Rearranges elements in a tensor of shape [B, C, D1 * r, D2 * r, ... DN * r]
     to a tensor of shape [B, C * r^N, D1, DN], where r is a downscale factor.
 
     Parameters
     ----------
-    downscale_factor : int 
+    downscale_factor : int
         factor to decrease spatial resolution by.
     """
     def __init__(self, downscale_factor):
         super().__init__()
         self.downscale_factor = downscale_factor
-        
+
     def forward(self, x):
         x = x.contiguous()                                                             # (B, C, H * r, B * r)
         batch_size, channels, *dims = x.size()
@@ -65,11 +67,11 @@ class PixelUnshuffle(nn.Module):
         reshape = [None] * (2 * ndims)
         reshape[::2] = out_dims
         reshape[1::2] = [self.downscale_factor for i in range(ndims)]
-        
+
         x = x.view(batch_size, channels, *reshape)                                     # (B, C, H, r, W, r)
-        
+
         permute = [None] * (2 * ndims)
-        permute[:2 * ndims] = range(3, 2 * ndims + 2, 2)      
+        permute[:2 * ndims] = range(3, 2 * ndims + 2, 2)
         permute[2 * ndims:] = range(2, 2 * ndims + 1, 2)
         x = x.permute(0, 1, *permute).contiguous()                                     # (B, C, r, r, H, W)
 
