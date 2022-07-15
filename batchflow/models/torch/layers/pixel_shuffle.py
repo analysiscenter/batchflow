95af291f168c972ed9beecf63d1efbe6343695ae
AnnaAltynova <altynanke@gmail.com>
2022-07-15 12:53:57 +0300

    fixed after review

--- batchflow/models/torch/layers/pixel_shuffle.py
+++ batchflow/models/torch/layers/pixel_shuffle.py
@@ -1,4 +1,4 @@
-""" Layer implementing Pixel Shuffle for an arbitrary dimensionality.
+""" Layers implementing Pixel Shuffle and Pixel Unshuffle for an arbitrary dimensionality.
 Shi et al. "`Real-Time Single Image and Video Super-Resolution
 Using an Efficient Sub-Pixel Convolutional Neural Network_
 <https://arxiv.org/pdf/1609.05158.pdf>`"
@@ -8,36 +8,40 @@ from torch import nn
 
 
 class PixelShuffle(nn.Module):
-    """ Rearranges elements in a tensor of shape [B, C * r^N, D1, D2, ... DN]
+    """ Extends `torch.nn.PixelShuffle` to and arbitrary number of dimensions.
+    Rearranges elements in a tensor of shape [B, C * r^N, D1, D2, ... DN]
     to a tensor of shape [B, C, D1 * r, D2 * r, ... DN * r]
     where r is an upscale factor.
 
     Parameters
     ----------
     upscale_factor : int
-        factor to increase spatial resolution by.
+        Factor to increase spatial resolution by.
     """
     def __init__(self, upscale_factor):
         super().__init__()
         self.upscale_factor = upscale_factor
 
     def forward(self, x):
-        batch_size, channels, *dims = x.size()                                         # (B, C * r^N, H, W)
+        batch_size, channels, *dims = x.size()                                         # (B, C * r^2, H, W)
         ndims = len(dims)
-        channels //= (self.upscale_factor**ndims)
+
+        if channels % (self.upscale_factor**ndims) != 0:
+            raise ValueError("PixelShuffle expects input of shape [B, C * r^N, D1, D2, ... DN]")
+        out_channels = channels // self.upscale_factor**ndims
 
         factor_expand = [self.upscale_factor] * ndims
         x = x.contiguous().view(                                                       # (B, C, r, r, H, W)
-            batch_size, channels, *factor_expand, *dims
+            batch_size, out_channels, *factor_expand, *dims
             )
 
-        permute = [None] * (2 * ndims)
-        permute[::2] = range(ndims + 2, 2 * ndims + 2)
-        permute[1::2] = range(2, ndims + 2)
-        x = x.permute(0, 1, *permute).contiguous()                                     # (B, C, H, r, W, r)
+        permute_dims = [None] * (2 * ndims)
+        permute_dims[::2] = range(ndims + 2, 2 * ndims + 2)
+        permute_dims[1::2] = range(2, ndims + 2)
+        x = x.permute(0, 1, *permute_dims).contiguous()                                # (B, C, H, r, W, r)
 
         out_dims = [dim * self.upscale_factor for dim in dims]
-        x = x.view(batch_size, channels, *out_dims).contiguous()                       # (B, C, H * r, W * r)
+        x = x.view(batch_size, out_channels, *out_dims).contiguous()                   # (B, C, H * r, W * r)
         return x
 
     def extra_repr(self):
@@ -46,13 +50,14 @@ class PixelShuffle(nn.Module):
 
 
 class PixelUnshuffle(nn.Module):
-    """ Rearranges elements in a tensor of shape [B, C, D1 * r, D2 * r, ... DN * r]
+    """ Extends `torch.nn.PixelUnShuffle` to and arbitrary number of dimensions.
+    Rearranges elements in a tensor of shape [B, C, D1 * r, D2 * r, ... DN * r]
     to a tensor of shape [B, C * r^N, D1, DN], where r is a downscale factor.
 
     Parameters
     ----------
     downscale_factor : int
-        factor to decrease spatial resolution by.
+        Factor to decrease spatial resolution by.
     """
     def __init__(self, downscale_factor):
         super().__init__()
@@ -63,17 +68,21 @@ class PixelUnshuffle(nn.Module):
         batch_size, channels, *dims = x.size()
         ndims = len(dims)
 
+        for dim in dims:
+            if dim % self.downscale_factor != 0:
+                raise ValueError("PixelUnshuffle expects input of shape [B, C, D1 * r, D2 * r, ... DN * r]")
         out_dims = [dim // self.downscale_factor for dim in dims]
-        reshape = [None] * (2 * ndims)
-        reshape[::2] = out_dims
-        reshape[1::2] = [self.downscale_factor for i in range(ndims)]
 
-        x = x.view(batch_size, channels, *reshape)                                     # (B, C, H, r, W, r)
+        shape_dims = [None] * 2 * ndims
+        shape_dims[::2] = out_dims
+        shape_dims[1::2] = [self.downscale_factor] * ndims
 
-        permute = [None] * (2 * ndims)
-        permute[:2 * ndims] = range(3, 2 * ndims + 2, 2)
-        permute[2 * ndims:] = range(2, 2 * ndims + 1, 2)
-        x = x.permute(0, 1, *permute).contiguous()                                     # (B, C, r, r, H, W)
+        x = x.view(batch_size, channels, *shape_dims)                                  # (B, C, H, r, W, r)
+
+        permute_dims = [None] * 2 * ndims
+        permute_dims[:2 * ndims] = range(3, 2 * ndims + 2, 2)
+        permute_dims[2 * ndims:] = range(2, 2 * ndims + 1, 2)
+        x = x.permute(0, 1, *permute_dims).contiguous()                                # (B, C, r, r, H, W)
 
         out_channels = channels * self.downscale_factor ** ndims
         x = x.view(batch_size, out_channels, *out_dims).contiguous()                   # (B, C * r^N, H, W)
