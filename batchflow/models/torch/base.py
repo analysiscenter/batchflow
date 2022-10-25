c173f2f8624f0d61e5bbf225efafd89105553d0c
AnnaAltynova <altynanke@gmail.com>
2022-10-25 13:32:35 +0000

    make pylint unsad

--- batchflow/models/torch/base.py
+++ batchflow/models/torch/base.py
@@ -1508,15 +1508,17 @@ class TorchModel(BaseModel, ExtractionMixin, OptimalBatchSizeMixin, Visualizatio
         Scalar values are aggregated by `mean`, array values are concatenated along the first (batch) axis.
         """
         result = []
-        for i, output_name in enumerate(outputs):
+        for i, _ in enumerate(outputs):
             chunked_output = [chunk_outputs[i] for chunk_outputs in chunked_outputs]
             if chunked_output[0].size != 1:
                 if len(chunked_output) == 1:
                     output_ = chunked_output[0]
                 elif isinstance(chunked_output[0], np.ndarray):
-                    output_ = np.concatenate([chunk_output[:chunk_size] for chunk_output, chunk_size in zip(chunked_output, chunk_sizes)], axis=0)
+                    output_ = np.concatenate([chunk_output[:chunk_size]
+                                              for chunk_output, chunk_size in zip(chunked_output, chunk_sizes)], axis=0)
                 else:
-                    output_ = torch.cat([chunk_output[:chunk_size] for chunk_output, chunk_size in zip(chunked_output, chunk_sizes)], dim=0)
+                    output_ = torch.cat([chunk_output[:chunk_size]
+                                         for chunk_output, chunk_size in zip(chunked_output, chunk_sizes)], dim=0)
                 result.append(output_)
             else:
                 result.append(np.mean(chunked_output))
